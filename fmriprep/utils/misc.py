d182e074e2285b5cb2585d388583b6d6c3d943d8
Ross Blair <rblair2@stanford.edu>
2016-04-11 14:46:33 -0700

    update epi_meta to store bold json

--- fmriprep/utils/misc.py
+++ fmriprep/utils/misc.py
@@ -49,6 +49,7 @@ def collect_bids_data(dataset)
                     modality = filename.split('_')[-1]
                     if 'bold.nii' in modality:
                         imaging_data[subject][session]['epi'] = scan_file
+                    if 'bold.json' in modality:
                         imaging_data[subject][session]['epi_meta'] = scan_file
                     elif 'sbref.nii' in modality:
                         imaging_data[subject][session]['sbref'] = scan_file
@@ -63,3 +64,60 @@ def collect_bids_data(dataset)
                     else:
                         pass
     return imaging_data
+
+# id we know the subject id and session name we can collect the data files for 
+# just that subject/sesssion
+def collect_sub_ses_data(dataset, subject, session):
+    imaging_data = preproc_inputs.copy()
+    if include_types is None:
+        # include all scan types by default
+        include_types = ['func', 'anat', 'fmap', 'dwi']
+
+    subjects = _walk_dir_for_prefix(dataset, subject)
+    if len(subjects) == 0:
+        raise GeneratorExit("No BIDS subjects found to examine.")
+
+    for subject in subjects:
+        subj_dir = os.path.join(dataset, subject)
+
+        sessions = _walk_dir_for_prefix(subj_dir, session)
+
+        for scan_type in include_types:
+            # seems easier to consider the case of multi-session vs.
+            # single session separately?
+            if len(sessions) > 0:
+                subject_sessions = [os.path.join(subject, x)
+                                    for x in sessions]
+            else:
+                subject_sessions = [subject]
+
+            for session in subject_sessions:
+                scan_files = glob(os.path.join(
+                    dataset, session, scan_type,
+                    '*'))
+
+                if len(scan_files) == 0:
+                    _no_files_warning(session)
+
+                for scan_file in scan_files:
+                    filename = scan_file.split('/')[-1]
+                    modality = filename.split('_')[-1]
+                    if 'bold.nii' in modality:
+                        imaging_data['epi'] = scan_file
+                    elif 'bold.json' in modality:
+                        imaging_data['epi_meta'] = scan_file
+                    elif 'sbref.nii' in modality:
+                        imaging_data['sbref'] = scan_file
+                    elif 'sbref_json' in modality:
+                        imaging_data['sbref_meta'] = scan_file
+                    elif 'T1W' in modality:
+                        imaging_data['t1'] = scan_file
+                    elif 'epi.nii' in modality:
+                        imaging_data['fieldmaps'].append(scan_file)
+                    elif 'epi.json' in modality:
+                        imaging_data['fieldmaps'].append(scan_file)
+                    else:
+                        pass
+    return imaging_data
+
+
