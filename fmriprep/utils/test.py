e1c8c3f75da305fdd7b4d4a5be25388b815f5c5e
Ross Blair <rblair2@stanford.edu>
2016-04-21 14:44:54 -0700

    add empty file structure that matches reduced AA connectivity dataset in bidsish format. Basic unitests for utils misc.py

---
+++ fmriprep/utils/test.py
@@ -0,0 +1,48 @@
+import json
+import misc
+import unittest
+
+class TestCollectBids(unittest.TestCase):
+    def setUp(self):
+        self.dataset = "../../test_data/aa_conn"
+        self.imaging_data = misc.collect_bids_data(self.dataset)
+
+    def test_epi(self):
+        epi_template = "{subject}/func/{subject}_task-rest_acq-LR_run-1_bold.nii.gz"
+        for subject in self.imaging_data:
+            for session in self.imaging_data[subject]:
+                self.assertIn(epi_template.format(subject=subject), 
+                              self.imaging_data[subject][session]['epi'])
+
+    def test_epi_meta(self):
+        epi_meta_template = "{subject}/func/{subject}_task-rest_acq-LR_run-1_bold.json"
+        for subject in self.imaging_data:
+            for session in self.imaging_data[subject]:
+                self.assertIn(epi_meta_template.format(subject=subject), 
+                              self.imaging_data[subject][session]['epi_meta'])
+
+    def test_sbref(self):
+        sbref_template = "{subject}/func/{subject}_task-rest_acq-LR_run-1_sbref.nii.gz"
+        for subject in self.imaging_data:
+            for session in self.imaging_data[subject]:
+                self.assertIn(sbref_template.format(subject=subject), 
+                              self.imaging_data[subject][session]['sbref'])
+
+    def test_sbref_meta(self):
+        sbref_meta_template = "{subject}/func/{subject}_task-rest_acq-LR_run-1_sbref.json"
+        for subject in self.imaging_data:
+            for session in self.imaging_data[subject]:
+                self.assertIn(sbref_meta_template.format(subject=subject), 
+                              self.imaging_data[subject][session]['sbref_meta'])
+    
+    def test_t1(self):
+        t1_template = "{subject}/anat/{subject}_run-1_T1w.nii.gz"
+        for subject in self.imaging_data:
+            for session in self.imaging_data[subject]:
+                self.assertIn(t1_template.format(subject=subject), 
+                              self.imaging_data[subject][session]['t1'])
+        
+if __name__ == '__main__':
+    unittest.main() 
+    #dataset = "../../test_data/aa_conn"
+    #imaging_data = misc.collect_bids_data(dataset)
