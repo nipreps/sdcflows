22a3de3db566ccba80a0b9d5a613fbc459409632
Ross Blair <rblair2@stanford.edu>
2016-05-10 16:48:40 -0700

    update next call on list iterator to be python 3 compatiable

--- fmriprep/viz/twelve_image_report_function_w_error.py
+++ fmriprep/viz/twelve_image_report_function_w_error.py
@@ -4,20 +4,20 @@ Created on Sun Jul 26 16:54:08 2015
 
 @author: craigmoodie
 """
-import os.path
-import pylab as plt
-from matplotlib.gridspec import GridSpec
-from matplotlib.backends.backend_pdf import PdfPages
-import matplotlib.image as mimage
 
 
 def generate_report(output_file, first_plot, second_plot, third_plot,
                     fifth_plot, sixth_plot, seventh_plot):
+    import os.path
+    import pylab as plt
+    from matplotlib.gridspec import GridSpec
+    from matplotlib.backends.backend_pdf import PdfPages
+    import matplotlib.image as mimage
 
     plots = ["output_file", "first_plot", "second_plot", "third_plot",
              "fifth_plot", "sixth_plot", "seventh_plot"]
-    error_image = "some/path/"
-    error = mimage.imread(error_image)
+    #  error_image = ""
+    #  error = mimage.imread(error_image)
     report = PdfPages(output_file)
     fig = plt.figure()
     grid = GridSpec(4, 3)
@@ -25,9 +25,10 @@ def generate_report(output_file, first_plot, second_plot, third_plot,
     plot_iterator = iter(plots)
     for i in range(0, 3):
         for j in range(0, 4):
-            plot = plot_iterator.next()
+            plot = next(plot_iterator)
             if plot is None:
-                plot = error
+                #  plot = error
+                continue
             img = mimage.imread(plot)
             ax = plt.subplot(grid[j, i])
             ax.imshow(img)
