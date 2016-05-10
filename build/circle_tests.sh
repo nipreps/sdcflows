c70807b401b3b77ddd8f90821114271b8f046ebc
oesteban <code@oscaresteban.es>
2016-05-10 13:34:49 -0700

    [TESTS] Add single-subject test

--- build/circle_tests.sh
+++ build/circle_tests.sh
@@ -3,4 +3,4 @@
 set -x
 set -e
 
-docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_fmriprep" oesteban/fmriprep -i /scratch/data/aa_conn -o outputs/ms-fmriprep/out -w outputs/ms-fmriprep/work
+docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_fmriprep" oesteban/fmriprep -B /scratch/data/AA_Conn -S sub-S8848XEU -o outputs/ms-fmriprep/out -w outputs/ms-fmriprep/work
