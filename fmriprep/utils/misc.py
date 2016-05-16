484c0dc9a474fe0862edc77468295343c4127052
Ross Blair <rblair2@stanford.edu>
2016-05-16 16:32:03 -0700

    start of rewrite of collect_bids_data

--- fmriprep/utils/misc.py
+++ fmriprep/utils/misc.py
@@ -2,24 +2,28 @@ from glob import glob
 import copy
 import os
 
-INPUTS_SPEC = {'fieldmaps': [], 'fieldmaps_meta': [], 'epi': '', 'epi_meta': '', 'sbref': '',
-                  'sbref_meta': '', 't1': ''}
+INPUTS_SPEC = {'fieldmaps': [], 'fieldmaps_meta': [], 'epi': '',
+               'epi_meta': '', 'sbref': '', 'sbref_meta': '', 't1': ''}
+
 
 def _walk_dir_for_prefix(target_dir, prefix):
     return [x for x in next(os.walk(target_dir))[1]
             if x.startswith(prefix)]
 
-def get_subject(bids_root, subject_id, session_id=None, run_id=None, include_types=None):
+
+def get_subject(bids_root, subject_id, session_id=None, run_id=None, 
+                include_types=None):
     """
     Returns the imaging_data structure for the subject subject_id.
     If session is None, then the BIDS structure is not multisession.
-    If run_id is None, it is assumed that the session does not have several runs.
+    If run_id is None, it is assumed that the session does not have several 
+    runs.
     """
     if include_types is None:
-        # include all scan types by default
-        include_types = ['func', 'anat', 'fmap']  # Please notice that dwi is not here
-
-    subject_data = collect_bids_data(bids_root, include_types=None)['sub-' + subject_id]
+        # Please notice that dwi is not here
+        include_types = ['func', 'anat', 'fmap']  
+    subject_data = collect_bids_data(bids_root, include_types=None)
+    subject_data = subject_data['sub-' + subject_id]
 
     if session_id is None:
         subject_data = subject_data[list(subject_data.keys())[0]]
@@ -31,6 +35,7 @@ def get_subject(bids_root, subject_id, session_id=None, run_id=None, include_typ
 
     return subject_data
 
+
 # if no scan_subject or scan_session are defined return all bids data for a
 # given bids directory. Otherwise just the data for a given subject or scan
 # can be returned
@@ -38,7 +43,6 @@ def collect_bids_data(dataset, include_types=None, scan_subject='sub-',
                       scan_session='ses-'):
     imaging_data = {}
     if include_types is None:
-        # include all scan types by default
         include_types = ['func', 'anat', 'fmap', 'dwi']
 
     subjects = _walk_dir_for_prefix(dataset, scan_subject)
@@ -64,9 +68,8 @@ def collect_bids_data(dataset, include_types=None, scan_subject='sub-',
             for session in subject_sessions:
                 if session not in imaging_data[subject]:
                     imaging_data[subject][session] = copy.deepcopy(INPUTS_SPEC)
-                scan_files = glob(os.path.join(
-                    dataset, session, scan_type,
-                    '*'))
+                scan_files = glob(os.path.join(dataset, session, scan_type, 
+                                               '*'))
 
                 for scan_file in scan_files:
                     filename = scan_file.split('/')[-1]
@@ -95,6 +98,86 @@ def collect_bids_data(dataset, include_types=None, scan_subject='sub-',
                         else:
                             pass
     return imaging_data
+
+
+class SubjectIDNotFound(Exception):
+    pass
+
+class SessionIDNotFound(Exception):
+    pass
+
+class RunIDNotFound(Exception):
+    pass
+
+def new_collect_bids_data(dataset, include_types=None, scan_subject='sub-', 
+                      scan_session='ses-'):
+    imaging_data = {}
+    if include_types is None:
+        include_types = ['func', 'anat', 'fmap', 'dwi']
+    file_lists = {}
+    for include_type in include_types:
+        file_lists[include_type] = []
+
+    subjects = _walk_dir_for_prefix(dataset, scan_subject)
+    if len(subjects) == 0:
+        raise GeneratorExit("No BIDS subjects found to examine.")
+
+    bids_files = []
+    for path, subdirs, files in os.walk('.'):
+        for name in files:
+            bids_files.append(os.path.join(path, name))
+
+    imaging_data = copy.deepcopy(INPUTS_SPEC)
+    for bids_file in bids_files:
+        filename = scan_file.split('/')[-1]
+        filename_parts = filename.split('_')
+        modality = filename_parts[-1]
+
+        file_type = None
+        for include_type in include_types:
+            type_match = re.match('/{}/'.format(include_type)
+            if type_match is not None
+                file_type = include_type
+
+        if sub_id is not None:
+            subject_match = re.match('/sub-{})/'.format(sub_id), bids_file)
+            if not subject_match:
+                raise SubjectIDNotFound
+
+        if ses_id is not None:
+            session_match = re.match('/ses-{}/'.format(ses_id), bids_file)
+            if not session_match:
+                raise SessionIDNotFound
+
+        elif run_id is not None and file_type not 'fmap':
+            run_match = re.match('_run-{}_'.format(run_id, bids_file)
+            if not run_match:
+                raise RunIDNotFound
+
+        for include_type in include_types:
+            if file_type is include_type:
+                file_lists[file_type].append(bids_file)
+        '''
+        if 'bold.nii' in modality:
+            imaging_data[subject][session]['epi'] = scan_file
+        elif 'bold.json' in modality:
+            imaging_data[subject][session]['epi_meta'] = scan_file
+        elif 'sbref.nii' in modality:
+            imaging_data[subject][session]['sbref'] = scan_file
+        elif 'sbref.json' in modality:
+            imaging_data[subject][session]['sbref_meta'] = scan_file
+        else:
+            pass
+        if 'epi.nii' in modality:
+            imaging_data['fieldmaps'].append(scan_file)
+        elif 'epi.json' in modality:
+            imaging_data['fieldmaps_meta'].append(scan_file)
+        elif 'T1w.nii' in modality:
+            imaging_data['t1'] = scan_file
+        else:
+            pass
+        '''
+        
 
 if __name__ == '__main__':
     pass
