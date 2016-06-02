0fbc5b78b5be3cc8b3dce86f8dbc04f9fd5c2621
oesteban <code@oscaresteban.es>
2016-06-02 11:23:49 -0700

    connecting to datasink

--- fmriprep/viz/pipeline_reports.py
+++ fmriprep/viz/pipeline_reports.py
@@ -51,11 +51,14 @@ def parcel_overlay(in_file, overlay_file, out_file):
 
 def stripped_brain_overlay(in_file, overlay_file, out_file):
     import os.path
+    import nibabel as nb
     import matplotlib as mpl
     mpl.use('Agg')
     from nilearn.plotting import plot_roi
-    mask_display = plot_roi(in_file, overlay_file, output_file=out_file,
-                            title=out_file, display_mode="ortho", dim=-1)
+    vmax = nb.load(in_file).get_data().reshape(-1).max()
+    mask_display = plot_roi(
+        in_file, overlay_file, output_file=out_file, title=out_file,
+        display_mode="ortho", dim=-1, alpha=.3, vmax=vmax + 1)
     #  mask_display.bg_img(overlay_file)
     #  mask_display.title(out_file, x=0.01, y=0.99, size=15, color=None,
     #                     bgcolor=None, alpha=1)
