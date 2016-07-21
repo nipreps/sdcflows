7a9b245c6f66d49d1ad47b65ecb335d2a5878a25
Shoshana Berleant <berleant@stanford.edu>
2016-07-21 19:35:10 +0000

    learnings from meeting with @oesteban

--- sdcflows/workflows/phase_diff_and_magnitude.py
+++ sdcflows/workflows/phase_diff_and_magnitude.py
@@ -26,7 +26,7 @@ class PhaseDiffAndMagnitudes(FieldmapDecider):
     def _make_workflow(name='phase_diff_and_magnitudes', interp='Linear',
                        fugue_params=dict(smooth3d=2.0)):
         """
-        Maps the B0 field as proposed by [Jezzard95]_. Phase
+        Phase
         unwrapping is performed using `PRELUDE
         <http://fsl.fmrib.ox.ac.uk/fsl/fsl-4.1.9/fugue/prelude.html>`_
         [Jenkinson03]_. Preparation of the fieldmap is performed reproducing the
@@ -36,12 +36,7 @@ class PhaseDiffAndMagnitudes(FieldmapDecider):
         .. warning:: Only SIEMENS format fieldmaps are supported.
         
         .. admonition:: References
-    
-        .. [Jezzard95] Jezzard P, and Balaban RS, `Correction for geometric
-        distortion in echo planar images from B0 field variations
-        <http://dx.doi.org/10.1002/mrm.1910340111>`_,
-        MRM 34(1):65-73. (1995). doi: 10.1002/mrm.1910340111.
-        
+
         .. [Jenkinson03] Jenkinson M., `Fast, automated, N-dimensional
         phase-unwrapping algorithm <http://dx.doi.org/10.1002/mrm.10354>`_,
         MRM 49(1):193-197, 2003, doi: 10.1002/mrm.10354.
@@ -53,19 +48,23 @@ class PhaseDiffAndMagnitudes(FieldmapDecider):
         outputnode = pe.Node(niu.IdentityInterface(
             fields=['out_file', 'out_vsm', 'out_warp']), name='outputnode')
 
-        r_params = _make_node_r_params()
-
-        eff_echo = pe.Node(niu.Function(function=_eff_t_echo,
-                                        input_names=['echospacing', 'acc_factor'],
-                                        output_names=['eff_echo']), name='EffEcho')
+        # ideally use mcflirt to align and then average w fsl something
         firstmag = pe.Node(fsl.ExtractROI(t_min=0, t_size=1), name='GetFirst')
+
+        # de-gradiant the fields ("illumination problem")
         n4 = pe.Node(ants.N4BiasFieldCorrection(dimension=3), name='Bias')
+
         bet = pe.Node(fsl.BET(frac=0.4, mask=True), name='BrainExtraction')
+
+        # uses mask from bet; outputs a mask
         dilate = pe.Node(fsl.maths.MathsCommand(
             nan2zeros=True, args='-kernel sphere 5 -dilM'), name='MskDilate')
+
+        # phase diff -> radians
         pha2rads = pe.Node(niu.Function(
             input_names=['in_file'], output_names=['out_file'],
             function=siemens2rads), name='PreparePhase')
+
         prelude = pe.Node(fsl.PRELUDE(process3d=True), name='PhaseUnwrap')
         rad2rsec = pe.Node(niu.Function(
             input_names=['in_file', 'delta_te'], output_names=['out_file'],
@@ -73,11 +72,7 @@ class PhaseDiffAndMagnitudes(FieldmapDecider):
         baseline = pe.Node(niu.Function(
             input_names=['in_file', 'index'], output_names=['out_file'],
             function=time_avg), name='Baseline')
-        
-        fmm2b0 = _make_node_fmm2b0()
-        
-        applyxfm = pe.Node(ants.ApplyTransforms(
-            dimension=3, interpolation=interp), name='FMp_to_B0')
+
         pre_fugue = pe.Node(fsl.FUGUE(save_fmap=True), name='PreliminaryFugue')
         demean = pe.Node(niu.Function(
             input_names=['in_file', 'in_mask'], output_names=['out_file'],
@@ -90,13 +85,7 @@ class PhaseDiffAndMagnitudes(FieldmapDecider):
             function=add_empty_vol), name='AddEmptyVol')
         vsm = pe.Node(fsl.FUGUE(save_shift=True, **fugue_params),
                       name="ComputeVSM")
-        split = pe.Node(fsl.Split(dimension='t'), name='SplitDWIs')
-        merge = pe.Node(fsl.Merge(dimension='t'), name='MergeDWIs')
 
-        unwarp = pe.MapNode(fsl.FUGUE(icorr=True, forward_warping=False),
-                            iterfield=['in_file'], name='UnwarpDWIs')
-        thres = pe.MapNode(fsl.Threshold(thresh=0.0), iterfield=['in_file'],
-                           name='RemoveNegative')
         vsm2dfm = _make_workflow_vsm2warp()
         
         wf = pe.Workflow(name=name)
@@ -157,36 +146,13 @@ class PhaseDiffAndMagnitudes(FieldmapDecider):
         return wf
 
     def _make_node_r_params():
+        # find these values in data--see bids spec
+        # delta_te from phase diff
+        # echospacing, acc_factor, enc_dir from sbref/epi
         epi_defaults = {'delta_te': 2.46e-3, 'echospacing': 0.77e-3,
-                        'acc_factor': 2, 'enc_dir': u'AP'}        
+                        'acc_factor': 2, 'enc_dir': u'AP'}
         return pe.Node(JSONFileGrabber(defaults=epi_defaults),
                            name='SettingsGrabber')
-
-    def _make_node_fmm2b0():
-        fmm2b0 = pe.Node(ants.Registration(output_warped_image=True),
-                         name="FMm_to_B0")
-
-        fmm2b0.inputs.transforms = ['Rigid'] * 2
-        fmm2b0.inputs.transform_parameters = [(1.0,)] * 2
-        fmm2b0.inputs.number_of_iterations = [[50], [20]]
-        fmm2b0.inputs.dimension = 3
-        fmm2b0.inputs.metric = ['Mattes', 'Mattes']
-        fmm2b0.inputs.metric_weight = [1.0] * 2
-        fmm2b0.inputs.radius_or_number_of_bins = [64, 64]
-        fmm2b0.inputs.sampling_strategy = ['Regular', 'Random']
-        fmm2b0.inputs.sampling_percentage = [None, 0.2]
-        fmm2b0.inputs.convergence_threshold = [1.e-5, 1.e-8]
-        fmm2b0.inputs.convergence_window_size = [20, 10]
-        fmm2b0.inputs.smoothing_sigmas = [[6.0], [2.0]]
-        fmm2b0.inputs.sigma_units = ['vox'] * 2
-        fmm2b0.inputs.shrink_factors = [[6], [1]]  # ,[1] ]
-        fmm2b0.inputs.use_estimate_learning_rate_once = [True] * 2
-        fmm2b0.inputs.use_histogram_matching = [True] * 2
-        fmm2b0.inputs.initial_moving_transform_com = 0
-        fmm2b0.inputs.collapse_output_transforms = True
-        fmm2b0.inputs.winsorize_upper_quantile = 0.995
-
-        return fmm2b0
 
     def _make_workflow_vsm2warp():
         vsm2dfm = vsm2warp()
