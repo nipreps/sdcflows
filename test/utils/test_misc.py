7029a53732c055380fd493d6ac02a1eb3843eb15
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-08-30 15:36:24 -0700

    passing misc tests

--- test/utils/test_misc.py
+++ test/utils/test_misc.py
@@ -10,22 +10,23 @@ from zipfile import ZipFile
 
 
 class TestCollectBids(unittest.TestCase):
-    fake_data_location = os.path.join("test", "fake_data")
-    subject_id = "100185"
+    fake_data_root_location = os.path.join("test", "fake_data")
+    fake_ds_location = os.path.join(fake_data_root_location,
+                                    "BIDS-examples-1-enh-ds054",
+                                    "ds054")
+    subject_id = "sub-100185"
 
     @classmethod
     def setUp(cls):
-        if not os.path.exists(cls.fake_data_location):
+        if not os.path.exists(cls.fake_data_root_location):
             print("Downloading test data")
             url = "http://github.com/chrisfilo/BIDS-examples-1/archive/enh/ds054.zip"
             with urlopen(url) as zipresp:
                 with ZipFile(BytesIO(zipresp.read())) as zfile:
-                    zfile.extractall(cls.fake_data_location)
+                    zfile.extractall(cls.fake_data_root_location)
 
         cls.imaging_data = {
-            cls.subject_id: misc.collect_bids_data(os.path.join(cls.fake_data_location,
-                                                                "BIDS-examples-1-enh-ds054",
-                                                               "ds054"),
+            cls.subject_id: misc.collect_bids_data(os.path.join(cls.fake_ds_location),
                                                    cls.subject_id,
                                                    spec=pkgr.resource_filename(
                                                        'fmriprep',
@@ -41,17 +42,17 @@ class TestCollectBids(unittest.TestCase):
         self.assertNotEqual(0, len(next(iter(self.imaging_data.values()))))
 
     def test_epi(self):
-        epi_template = "{subject}/func/{subject}_task-rest_acq-LR_run-1_bold.nii.gz"
-        self.assert_key_exists(epi_template, 'epi')
+        epi_template = self.fake_ds_location + "/{subject}/func/{subject}_task-machinegame_run-06_bold.nii.gz"
+        self.assert_key_exists(epi_template, 'func')
 
     def test_sbref(self):
-        sbref_template = (self.fake_data_location + "/{subject}/func/"
-                          "{subject}_task-rest_acq-LR_run-1_sbref.nii.gz")
+        sbref_template = (self.fake_ds_location + "/{subject}/func/"
+                          "{subject}_task-machinegame_run-06_sbref.nii.gz")
         self.assert_key_exists(sbref_template, 'sbref')
 
-    def test_t1(self):
-        t1_template = "{subject}/anat/{subject}_run-1_T1w.nii.gz"
-        self.assert_key_exists(t1_template, 't1')
+    def test_t1w(self):
+        t1_template = self.fake_ds_location + "/{subject}/anat/{subject}_T1w.nii.gz"
+        self.assert_key_exists(t1_template, 't1w')
 
     def test_fieldmaps(self):
         fieldmap_pattern = r"{0}\/fmap\/{0}_dir-[0-9]+_run-[0-9]+_epi\.nii\.gz"
@@ -62,16 +63,14 @@ class TestCollectBids(unittest.TestCase):
     def assert_fieldmap_files_exist(self, pattern, key):
         for subject in self.imaging_data:
             search_pattern = pattern.format(subject)
-            for session in self.imaging_data[subject]:
-                for fieldmap in self.imaging_data[subject][session][key]:
-                    match = re.search(search_pattern, fieldmap)
-                    self.assertTrue(match)
+            for fieldmap in self.imaging_data[subject][key]:
+                match = re.search(search_pattern, fieldmap)
+                self.assertTrue(match)
 
     def assert_key_exists(self, template, key):
         for subject in self.imaging_data:
-            for session in self.imaging_data[subject]:
-                self.assertIn(template.format(subject=subject),
-                              self.imaging_data[subject][session][key])
+            self.assertIn(template.format(subject=subject),
+                          self.imaging_data[subject][key])
         
 if __name__ == '__main__':
     unittest.main() 
