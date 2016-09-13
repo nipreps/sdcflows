91705af78e1e2b90ead629246584c2053e67f83b
oesteban <code@oscaresteban.es>
2016-09-13 11:31:30 -0700

    fix install scripts

--- fmriprep/info.py
+++ fmriprep/info.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
 Base module variables
 """
+from __future__ import unicode_literals
 
 __version__ = '0.1.1a3'
 __author__ = 'The CRN developers'
@@ -39,3 +39,29 @@ such as skull stripping and template registration. This pipeline was designed to
 implementation for each state of preprocessing, and will be updated as newer and better neuroimaging software
 become available.
 """
+
+REQUIRES = [
+    'numpy',
+    'lockfile',
+    'future',
+    'scikit-learn',
+    'matplotlib',
+    'nilearn',
+    'sklearn',
+    'nibabel',
+    'niworkflows>=0.0.3a3',
+    'grabbit'
+]
+
+LINKS_REQUIRES = [
+    'git+https://github.com/nipy/nipype.git@master#egg=nipype'
+]
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: MRI processing',
+    'Topic :: Scientific/Engineering :: Biomedical Imaging',
+    'License :: OSI Approved :: 3-clause BSD License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5'
+]
