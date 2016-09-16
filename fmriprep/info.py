80e5f1a8842c6f0f59bbdc46bff1fe34b928bb1c
oesteban <code@oscaresteban.es>
2016-09-16 10:51:41 -0700

    remove last modified, move download_url to info.py

--- fmriprep/info.py
+++ fmriprep/info.py
@@ -40,6 +40,9 @@ implementation for each state of preprocessing, and will be updated as newer and
 become available.
 """
 
+DOWNLOAD_URL = ('https://pypi.python.org/packages/source/f/fmriprep/' +
+                'fmriprep-%s.tar.gz').format('__version__')
+
 REQUIRES = [
     'numpy',
     'lockfile',
