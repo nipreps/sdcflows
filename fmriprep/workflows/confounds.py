7d268ef2c3fcdd39343a069e54f7553d836bb29c
Shoshana Berleant <berleant@stanford.edu>
2016-10-01 03:01:34 +0000

    add gather confounds func

--- fmriprep/workflows/confounds.py
+++ fmriprep/workflows/confounds.py
@@ -5,7 +5,6 @@ Calculates frame displacement, segment regressors, global regressor, dvars, aCom
 from nipype.interfaces import utility, nilearn
 from nipype.algorithms import confounds
 from nipype.pipeline import engine as pe
-from nipype.algorithms import misc
 
 def discover_wf(name="ConfoundDiscoverer"):
     ''' All input fields are required.
@@ -36,6 +35,10 @@ def discover_wf(name="ConfoundDiscoverer"):
     dvars = pe.Node(confounds.ComputeDVARS(), name="ComputeDVARS", save_all=True,
                     remove_zerovariance=True)
 
+    concat = pe.Node(utility.Function(function=_gather_confounds, input_names=['signals', 'dvars'],
+                                      output_names=['combined_out']),
+                     name="ConcatConfounds")
+
     workflow = pe.Workflow(name=name)
     workflow.connect([
         (inputnode, signals, [('fmri_file', 'in_file'),
@@ -43,7 +46,31 @@ def discover_wf(name="ConfoundDiscoverer"):
         (inputnode, dvars, [('fmri_file', 'in_file'),
                             ('epi_mask', 'in_mask')]),
 
-        (signals, outputnode, [('out_file', 'confounds_file')])
+        (signals, concat, [('out_file', 'signals')]),
+        (dvars, concat, [('out_all', 'dvars')]),
+
+        (concat, outputnode, [('combined_out', 'confounds_file')])
     ])
 
     return workflow
+
+def _gather_confounds(signals=None, dvars=None):
+    ''' load confounds from the filenames, concatenate together horizontally, and re-save '''
+    import pandas as pd
+
+    all_files = [confound for confound in [signals, dvars] if confound != None]
+
+    # make sure there weren't any name conflicts
+    if len(all_files) != len(set(all_files)):
+        raise RuntimeError('A confound-calculating node over-wrote another confound-calculating'
+                           'node\'s results! Check ' + str(all_files))
+
+    confounds = pd.DataFrame()
+    for file_name in all_files: # assumes they all have headings already
+        new = pd.read_csv(file_name, sep="\t")
+        confounds = pd.concat((confounds, new), axis=1)
+
+    combined_out = 'confounds.tsv'
+    confounds.to_csv(combined_out, sep="\t")
+
+    return combined_out
--- test/workflows/test_confounds.py
+++ test/workflows/test_confounds.py
@@ -1,7 +1,9 @@
 ''' Testing module for fmriprep.workflows.confounds '''
 import unittest
+import mock
+import pandas as pd
 
-from fmriprep.workflows.confounds import discover_wf
+from fmriprep.workflows.confounds import discover_wf, _gather_confounds
 
 from test.workflows.utilities import TestWorkflow
 
@@ -24,3 +26,33 @@ class TestConfounds(TestWorkflow):
 
         # Make sure mandatory inputs are set
         self.assert_inputs_set(workflow, {'outputnode': ['confounds_file']})
+
+    def test_gather_confounds_err(self):
+        # set up
+        signals = "signals.tsv"
+        dvars = "signals.tsv"
+
+        # run & assert
+        with self.assertRaisesRegexp(RuntimeError, "confound"):
+            _gather_confounds(signals, dvars)
+
+    @mock.patch('pandas.read_csv')
+    @mock.patch.object(pd.DataFrame, 'to_csv', autospec=True)
+    @mock.patch.object(pd.DataFrame, '__eq__', autospec=True,
+                       side_effect=lambda me, them: me.equals(them))
+    def test_gather_confounds(self, df_equality, mock_df, mock_csv_reader):
+        # set up
+        signals = "signals.tsv"
+        dvars = "dvars.tsv"
+
+        mock_csv_reader.side_effect = [pd.DataFrame({'a': [0.1]}), pd.DataFrame({'b': [0.2]})]
+
+        # run
+        _gather_confounds(signals, dvars)
+
+        # assert
+        calls = [mock.call(confounds, sep="\t") for confounds in [signals, dvars]]
+        mock_csv_reader.assert_has_calls(calls)
+
+        confounds = pd.DataFrame({'a': [0.1], 'b': [0.2]})
+        mock_df.assert_called_once_with(confounds, "confounds.tsv", sep="\t")
