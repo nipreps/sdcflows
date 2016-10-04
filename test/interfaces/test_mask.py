3067e53338c84e100cb2ab0a50e8bb186ebfb33d
Shoshana Berleant <berleant@stanford.edu>
2016-10-04 05:27:23 +0000

    m

--- test/interfaces/test_mask.py
+++ test/interfaces/test_mask.py
@@ -1,11 +1,11 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 ''' Testing module for fmriprep.interfaces.mask '''
+import os
 import unittest
 import mock
 import nibabel as nb
 import numpy as np
-import os
 
 from fmriprep.interfaces.mask import BinarizeSegmentation
 
