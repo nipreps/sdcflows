493b35a56847e4180f2809253b8c77324fa2f452
oesteban <code@oscaresteban.es>
2016-10-05 10:13:40 -0700

    Several improvements handling folders and writing artifacts
    - Do not exit when makedirs on an existing folder (mriqc/#111)
    - Circle: move outputs to ~/dsXXX/out folder
    - Circle: do not try to add work/ under artifacts
    - Move default log folder under out/ (so circle catches crashes as artifacts).

--- fmriprep/run_workflow.py
+++ fmriprep/run_workflow.py
@@ -3,7 +3,7 @@
 # @Author: oesteban
 # @Date:   2015-11-19 16:44:27
 # @Last Modified by:   oesteban
-# @Last Modified time: 2016-10-03 09:06:10
+# @Last Modified time: 2016-10-05 10:11:51
 """
 fMRI preprocessing workflow
 =====
@@ -72,6 +72,7 @@ def main():
 def create_workflow(opts):
     import logging
     from nipype import config as ncfg
+    from fmriprep.utils import check_folder
     from fmriprep.workflows import base as fwb
     from fmriprep.workflows.base import base_workflow_enumerator
 
@@ -95,23 +96,15 @@ def create_workflow(opts):
     if opts.ants_nthreads is not None:
         settings['ants_threads'] = opts.ants_nthreads
 
-    log_dir = op.join(settings['work_dir'], 'log')
+    log_dir = op.join(settings['output_dir'], 'log')
+    derivatives = op.join(settings['output_dir'], 'derivatives')
 
     # Check and create output and working directories
-    # Using locks to prevent https://github.com/poldracklab/mriqc/issues/111
-    with LockFile('.fmriprep-folders-lock'):
-        if not op.exists(settings['output_dir']):
-            os.makedirs(settings['output_dir'])
-
-        derivatives = op.join(settings['output_dir'], 'derivatives')
-        if not op.exists(derivatives):
-            os.makedirs(derivatives)
-
-        if not op.exists(settings['work_dir']):
-            os.makedirs(settings['work_dir'])
-
-        if not op.exists(log_dir):
-            os.makedirs(log_dir)
+    # Using check_folder to prevent https://github.com/poldracklab/mriqc/issues/111
+    check_folder(settings['output_dir'])
+    check_folder(settings['work_dir'])
+    check_folder(derivatives)
+    check_folder(log_dir)
 
     logger.addHandler(logging.FileHandler(op.join(log_dir, 'run_workflow')))
 
