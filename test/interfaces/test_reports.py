4bfa1247bcf55fbfb37a90e13090ac697fb1d186
Shoshana Berleant <berleant@stanford.edu>
2016-11-16 21:00:24 +0000

    use nipype's existing tempdir context manager

--- test/interfaces/test_reports.py
+++ test/interfaces/test_reports.py
@@ -1,13 +1,10 @@
 import os
-import shutil
-import tempfile
 import unittest
 
-from nipype.pipeline import engine as pe
+from nipype.utils.tmpdirs import InTemporaryDirectory
 from niworkflows.data.getters import get_mni_template_ras
 
 from fmriprep.interfaces.reports import BETRPT, FLIRTRPT, RegistrationRPT
-from test.utils.tempdir import in_temporary_directory
 
 MNI_DIR = get_mni_template_ras()
 
@@ -15,15 +12,15 @@ class TestFLIRTRPT(unittest.TestCase):
     def setUp(self):
         self.out_file = "test_flirt.nii.gz"
 
-    @in_temporary_directory
     def test_known_file_out(self):
-        template = os.path.join(MNI_DIR, 'MNI152_T1_1mm.nii.gz')
-        flirt_rpt = FLIRTRPT(generate_report=True, in_file=template,
-                             reference=template, out_file=self.out_file)
-        flirt_rpt.run()
-        html_report = flirt_rpt.aggregate_outputs().html_report
-        self.assertTrue(os.path.isfile(html_report), 'HTML report exists at {}'
-                        .format(html_report))
+        with InTemporaryDirectory():
+            template = os.path.join(MNI_DIR, 'MNI152_T1_1mm.nii.gz')
+            flirt_rpt = FLIRTRPT(generate_report=True, in_file=template,
+                                 reference=template, out_file=self.out_file)
+            flirt_rpt.run()
+            html_report = flirt_rpt.aggregate_outputs().html_report
+            self.assertTrue(os.path.isfile(html_report), 'HTML report exists at {}'
+                            .format(html_report))
 
 class TestBETRPT(unittest.TestCase):
     ''' tests it using mni as in_file '''
@@ -44,10 +41,10 @@ class TestBETRPT(unittest.TestCase):
         anyway (using arbitrary volume) '''
         pass
 
-    @in_temporary_directory
     def _smoke(self, bet_interface):
-        bet_interface.run()
+        with InTemporaryDirectory():
+            bet_interface.run()
 
-        html_report = bet_interface.aggregate_outputs().html_report
-        self.assertTrue(os.path.isfile(html_report), 'HTML report exists at {}'
-                        .format(html_report))
+            html_report = bet_interface.aggregate_outputs().html_report
+            self.assertTrue(os.path.isfile(html_report), 'HTML report exists at {}'
+                            .format(html_report))
