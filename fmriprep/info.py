7430e877b96f0863ebf1bf1553f49f0f5079835d
Ross Blair <rosswilsonblair@gmail.com>
2016-12-01 15:20:24 -0800

    fix niworkflows and nipype deps for fmriprep docker install

--- fmriprep/info.py
+++ fmriprep/info.py
@@ -54,16 +54,14 @@ REQUIRES = [
     'nibabel',
     'pandas',
     'grabbit',
-    'nipype',
     'pybids',
     'nitime',
-    'niworkflows'
+    'niworkflows',
+    'nipype'
 ]
 
 LINKS_REQUIRES = [
-    'git+https://github.com/nipy/nipype.git@master#egg=nipype',
     'git+https://github.com/incf/pybids.git@master#egg=pybids',
-    'git+https://github.com/poldracklab/niworkflows.git@master#egg=niworkflows',
 ]
 
 TESTS_REQUIRES = [
