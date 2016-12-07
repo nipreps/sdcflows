1567e9a7979531747e551c1c30618639314654b8
Ross Blair <rosswilsonblair@gmail.com>
2016-12-06 18:37:47 -0800

    if a run/task/ses etc can be determined for a set of files then go ahead and group them together in reports

--- fmriprep/viz/reports.py
+++ fmriprep/viz/reports.py
@@ -15,22 +15,72 @@ class Element(object):
         self.file_pattern = re.compile(file_pattern)
         self.title = title
         self.description = description
-        self.files = []
         self.files_contents = []
-        self.files_subjects = []
-
 
 class SubReport(object):
 
-    def __init__(self, name, elements, **kwargs):
+    def __init__(self, name, elements, title=''):
         self.name = name
+        self.title = title
         self.elements = []
+        self.run_reports = []
         for e in elements:
             element = Element(**e)
             self.elements.append(element)
 
     def order_by_run(self):
-        pass
+        run_reps = {}
+        for elem_index in range(len(self.elements) - 1, -1, -1):
+            element = self.elements[elem_index]
+            for index in range(len(element.files_contents) - 1, -1, -1):
+                filename = element.files_contents[index][0]
+                file_contents = element.files_contents[index][1]
+                name, title = self.generate_name_title(filename)
+                if not name:
+                    continue
+                new_elem = {'name': element.name, 'file_pattern': element.file_pattern,
+                            'title': element.title, 'description': element.description}
+                try:
+                    new_element = Element(**new_elem)
+                    run_reps[name].elements.append(new_element)
+                    run_reps[name].elements[-1].files_contents.append((filename, file_contents))
+                except KeyError:
+                    run_reps[name] = SubReport(name, [new_elem], title=title)
+                    run_reps[name].elements[0].files_contents.append((filename, file_contents))
+                #del element.files_contents[index]
+            #if element.files_contents is None:
+            #    del self.elements[elem_index]
+        keys = run_reps.keys()
+        keys.sort()
+        for key in keys:
+            self.run_reports.append(run_reps[key])
+
+    def generate_name_title(self, filename):
+        fname = os.path.basename(filename)
+        expr = re.compile('^sub-(?P<subject_id>[a-zA-Z0-9]+)(_ses-(?P<session_id>[a-zA-Z0-9]+))?'
+                          '(_task-(?P<task_id>[a-zA-Z0-9]+))?(_acq-(?P<acq_id>[a-zA-Z0-9]+))?'
+                          '(_rec-(?P<rec_id>[a-zA-Z0-9]+))?(_run-(?P<run_id>[a-zA-Z0-9]+))?')
+        outputs = expr.search(fname)
+        if outputs:
+            outputs = outputs.groupdict()
+        else:
+            return None, None
+            
+        name = '{session}{task}{acq}{rec}{run}'.format(
+            session="_ses-" + outputs['session_id'] if outputs['session_id'] else '',
+            task="_task-" + outputs['task_id'] if outputs['task_id'] else '',
+            acq="_acq-" + outputs['acq_id'] if outputs['acq_id'] else '',
+            rec="_rec-" + outputs['rec_id'] if outputs['rec_id'] else '',
+            run="_run-" + outputs['run_id'] if outputs['run_id'] else ''
+        )
+        title = '{session}{task}{acq}{rec}{run}'.format(
+            session=" Session: " + outputs['session_id'] if outputs['session_id'] else '',
+            task=" Task: " + outputs['task_id'] if outputs['task_id'] else '',
+            acq=" Acquisition: " + outputs['acq_id'] if outputs['acq_id'] else '',
+            rec=" Reconstruction: " + outputs['rec_id'] if outputs['rec_id'] else '',
+            run=" Run: " + outputs['run_id'] if outputs['run_id'] else ''
+        )
+        return name, title
 
 
 class Report(object):
@@ -61,13 +111,14 @@ class Report(object):
                 f = os.path.join(root, f)
                 for sub_report in self.sub_reports:
                     for element in sub_report.elements:
-                        ext = f.split('.')[-1] 
+                        ext = f.split('.')[-1]
                         if element.file_pattern.search(f) and (ext == 'svg' or ext == 'html'):
-                            element.files.append(f)
                             with open(f) as fp:
                                 content = fp.read()
                                 content = '\n'.join(content.split('\n')[1:])
                                 element.files_contents.append((f, content))
+        for sub_report in self.sub_reports:
+            sub_report.order_by_run()
 
     def generate_report(self):
         searchpath = pkgrf('fmriprep', '/')
