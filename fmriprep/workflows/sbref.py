4972a3fc4cc44147a2724d26e51af96e71cb708a
Ross Blair <rosswilsonblair@gmail.com>
2016-12-07 10:05:28 -0800

    change outpath for sbref report derivatives to reports

--- fmriprep/workflows/sbref.py
+++ fmriprep/workflows/sbref.py
@@ -51,7 +51,7 @@ def sbref_preprocess(name='SBrefPreprocessing', settings=None):
 
     ds_report = pe.Node(
         DerivativesDataSink(base_directory=settings['output_dir'],
-                            suffix='sbref_bet', out_path_base='report'),
+                            suffix='sbref_bet', out_path_base='reports'),
         name='DS_Report'
     )
 
@@ -169,7 +169,7 @@ def sbref_t1_registration(name='SBrefSpatialNormalization', settings=None):
 
     ds_report = pe.Node(
         DerivativesDataSink(base_directory=settings['output_dir'],
-                            suffix='sbref_t1_flt_bbr', out_path_base='report'),
+                            suffix='sbref_t1_flt_bbr', out_path_base='reports'),
         name='DS_Report'
     )
 
