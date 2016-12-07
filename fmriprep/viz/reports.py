c2c93472f308e27799e482b6b7f72f894f3a12cc
Ross Blair <rosswilsonblair@gmail.com>
2016-12-07 11:09:32 -0800

    search for reportlets from directory where subject id was found not root of reports dir

--- fmriprep/viz/reports.py
+++ fmriprep/viz/reports.py
@@ -142,8 +142,9 @@ def run_reports(out_dir):
         dir = root.split('/')[-1]
         try:
             subject = re.search('^(?P<subject_id>sub-[a-zA-Z0-9]+)$', dir).group()
+            print(subject)
             out_filename = '{}{}'.format(subject, '.html')
-            report = Report(path, config, out_dir, out_filename)
+            report = Report(root, config, out_dir, out_filename)
             report.generate_report()
         except AttributeError:
             continue
