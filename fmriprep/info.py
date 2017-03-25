56bb5cbd6f7923d8df0da0cb8c554100ee44e727
Oscar Esteban <code@oscaresteban.es>
2017-03-24 23:57:05 -0700

    enable cython extension

--- fmriprep/info.py
+++ fmriprep/info.py
@@ -24,6 +24,12 @@ DOWNLOAD_URL = (
     'https://pypi.python.org/packages/source/{name[0]}/{name}/{name}-{ver}.tar.gz'.format(
         name=__packagename__, ver=__version__))
 
+SETUP_REQUIRES = [
+    'setuptools>=18.0',
+    'numpy',
+    'cython',
+]
+
 REQUIRES = [
     'numpy',
     'lockfile',
