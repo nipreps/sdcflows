8cbd87d0c93c6bce94732e478b8394fdc248984f
Oscar Esteban <code@oscaresteban.es>
2017-03-25 12:51:39 -0700

    connecting fieldmap estimation

--- sdcflows/workflows/base.py
+++ sdcflows/workflows/base.py
@@ -28,13 +28,24 @@ LOGGER = logging.getLogger('workflow')
 def fmap_estimator(subject_data, settings=None):
     """
     This workflow selects the fieldmap estimation data available for the subject and
-    returns the estimated fieldmap in Hz, along with a corresponding reference image.
+    returns the estimated fieldmap in mm, along with a corresponding reference image.
+    Current implementation applies the first type of fieldmap estimation found in the
+    following order of precedence:
 
-    Any estimation workflow must produce three outputs:
+      * "Natural" fieldmaps
+      * Phase-difference fieldmaps
+      * PEPolar fieldmaps.
 
-      * fmap: The estimated fieldmap itself IN UNITS OF Hz.
-      * fmap_ref: the anatomical reference for the fieldmap (magnitude image, corrected SEm, etc.)
-      * fmap_mask: a brain mask for the fieldmap
+
+    Outputs
+    -------
+
+      fmap
+        The estimated fieldmap itself IN UNITS OF Hz.
+      fmap_ref
+        the anatomical reference for the fieldmap (magnitude image, corrected SEm, etc.)
+      fmap_mask
+        a brain mask for the fieldmap
 
 
     """
@@ -48,63 +59,42 @@ def fmap_estimator(subject_data, settings=None):
     outputnode = pe.Node(niu.IdentityInterface(
         fields=['fmap', 'fmap_ref', 'fmap_mask']), name='outputnode')
 
-    estimator_wfs = []
-    if any(['phase' in fname for fname in subject_data['fmap']]):
-        LOGGER.info('Fieldmap estimation: phase-difference images found')
-        from .phdiff import phdiff_workflow
-        phwf = phdiff_workflow(settings=settings)
-        # set inputs
-        phwf.inputs.inputnode.input_images = subject_data['fmap']
-        estimator_wfs.append(phwf)
-
+    estimator_wf = None
     if any(['fieldmap' in fname for fname in subject_data['fmap']]):
-        LOGGER.info('Fieldmap estimation: fieldmap images found')
         from .fmap import fmap_workflow
+        LOGGER.info('Fieldmap estimation: fieldmap images found')
         fmapwf = fmap_workflow(settings=settings)
         # set inputs
         fmapwf.inputs.inputnode.input_images = subject_data['fmap']
-        estimator_wfs.append(fmapwf)
+        estimator_wf = fmapwf
 
-    # PEPOLAR disabled for now
-    # if any(['epi' in fname for fname in subject_data['fmap']]):
-    #     LOGGER.info('Fieldmap estimation: phase-encoding images found')
-    #     from .pepolar import pepolar_workflow
-    #     pewf = pepolar_workflow(settings=settings)
-    #     # set inputs
-    #     pewf.inputs.inputnode.input_images = subject_data['fmap'] + subject_data['sbref']
-    #     estimator_wfs.append(pewf)
+    elif any(['phase' in fname for fname in subject_data['fmap']]):
+        from .phdiff import phdiff_workflow
+        LOGGER.info('Fieldmap estimation: phase-difference images found')
+        phwf = phdiff_workflow(settings=settings)
+        # set inputs
+        phwf.inputs.inputnode.input_images = subject_data['fmap']
+        estimator_wf = phwf
 
-    if not estimator_wfs:
+    elif any(['epi' in fname for fname in subject_data['fmap']]):
+        from .pepolar import pepolar_workflow
+        LOGGER.info('Fieldmap estimation: phase-encoding images found')
+        pewf = pepolar_workflow(settings=settings)
+        # set inputs
+        pewf.inputs.inputnode.input_images = subject_data['fmap'] + subject_data['sbref']
+        estimator_wf = pewf
+
+    if estimator_wf is None:
         LOGGER.warn(
             'Fielmap data found, but no estimation workflow could be built for it. '
             'Fieldmap data available = [%s]', ', '.join(subject_data['fmap']))
         return None
 
-    if len(estimator_wfs) > 1:
-        # Average estimated workflows (requires registration)
-        mrgfmaps = pe.Node(niu.Merge(len(estimator_wfs)), name='MergeFMaps')
-        mrgrefs = pe.Node(niu.Merge(len(estimator_wfs)), name='MergeRefs')
-        mrgmask = pe.Node(niu.Merge(len(estimator_wfs)), name='MergeMasks')
-
-        for i, est in enumerate(estimator_wfs):
-            workflow.connect(est, 'outputnode.fmap', mrgfmaps, 'in%d' % (i+1))
-            workflow.connect(est, 'outputnode.fmap_ref', mrgrefs, 'in%d' % (i+1))
-            workflow.connect(est, 'outputnode.fmap_mask', mrgmask, 'in%d' % (i+1))
-
-        workflow.connect([
-            (mrgfmaps, outputnode, [('out', 'fmap')]),
-            (mrgrefs, outputnode, [('out', 'fmap_ref')]),
-            (mrgmask, outputnode, [('out', 'fmap_mask')])
-        ])
-    else:
-        workflow.connect([
-            (estimator_wfs[0], outputnode, [
-                ('outputnode.fmap', 'fmap'),
-                ('outputnode.fmap_ref', 'fmap_ref'),
-                ('outputnode.fmap_mask', 'fmap_mask')])
-        ])
+    workflow.connect([
+        (estimator_wf, outputnode, [
+            ('outputnode.fmap', 'fmap'),
+            ('outputnode.fmap_ref', 'fmap_ref'),
+            ('outputnode.fmap_mask', 'fmap_mask')])
+    ])
 
     return workflow
-
-def _tolist(in_value):
-    return [in_value]
