e6f21546616e13ee3a0e32eb61d1a9a427f55b9e
Christopher J. Markiewicz <markiewicz@stanford.edu>
2017-06-21 14:18:09 -0400

    Update dev version, changelog

--- fmriprep/info.py
+++ fmriprep/info.py
@@ -6,7 +6,7 @@ Base module variables
 """
 from __future__ import unicode_literals
 
-__version__ = '0.4.7-dev'
+__version__ = '0.5.1-dev'
 __author__ = 'The CRN developers'
 __copyright__ = 'Copyright 2017, Center for Reproducible Neuroscience, Stanford University'
 __credits__ = ['Craig Moodie', 'Ross Blair', 'Oscar Esteban', 'Chris Gorgolewski',
