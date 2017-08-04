ccf23e5f5e3edca4601ddd31086c646804a7b3cb
oesteban <code@oscaresteban.es>
2017-08-04 10:34:31 -0700

    use runtime.cwd then appropriate

--- sdcflows/interfaces/fmap.py
+++ sdcflows/interfaces/fmap.py
@@ -8,7 +8,6 @@ Interfaces to deal with the various types of fieldmap sources
 """
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-import os
 from builtins import range
 import numpy as np
 import nibabel as nb
@@ -67,16 +66,16 @@ class FieldEnhance(SimpleInterface):
                 mask = sim.binary_erosion(
                     mask, struc,
                     iterations=self.inputs.mask_erode
-                    ).astype(np.uint8)  # pylint: disable=no-member
+                ).astype(np.uint8)  # pylint: disable=no-member
 
         self._results['out_file'] = fname_presuffix(
-            self.inputs.in_file, suffix='_enh', newpath=os.getcwd())
+            self.inputs.in_file, suffix='_enh', newpath=runtime.cwd)
         datanii = nb.Nifti1Image(data, fmap_nii.affine, fmap_nii.header)
 
         if self.inputs.unwrap:
             data = _unwrap(data, self.inputs.in_magnitude, mask)
             self._results['out_unwrapped'] = fname_presuffix(
-                self.inputs.in_file, suffix='_unwrap', newpath=os.getcwd())
+                self.inputs.in_file, suffix='_unwrap', newpath=runtime.cwd)
             nb.Nifti1Image(data, fmap_nii.affine, fmap_nii.header).to_filename(
                 self._results['out_unwrapped'])
 
