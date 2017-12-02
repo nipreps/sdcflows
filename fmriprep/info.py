2f922612e029034e19f69c1ad10f41a9a112d3fb
Oscar Esteban <code@oscaresteban.es>
2017-12-01 21:08:10 -0800

    [skip ci] Version bump

--- fmriprep/info.py
+++ fmriprep/info.py
@@ -5,7 +5,7 @@
 Base module variables
 """
 
-__version__ = '1.0.0-rc13'
+__version__ = '1.0.0-rc14-dev'
 __author__ = 'The CRN developers'
 __copyright__ = 'Copyright 2017, Center for Reproducible Neuroscience, Stanford University'
 __credits__ = ['Craig Moodie', 'Ross Blair', 'Oscar Esteban', 'Chris Gorgolewski',
