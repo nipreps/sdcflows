1a280b780a80878679e1bd66535d8fa1a371d61b
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-04-29 14:14:02 -0400

    TEST: Use bold_reference_wf to generate reference before enhancing

--- fmriprep/workflows/bold/tests/test_utils.py
+++ fmriprep/workflows/bold/tests/test_utils.py
@@ -3,7 +3,7 @@ import pytest
 
 import numpy as np
 from nilearn.image import load_img
-from ..utils import init_enhance_and_skullstrip_bold_wf
+from ..utils import init_bold_reference_wf
 
 
 def symmetric_overlap(img1, img2):
@@ -16,12 +16,13 @@ def symmetric_overlap(img1, img2):
     return overlap / np.sqrt(total1 * total2)
 
 
+@pytest.skip
 def test_masking(input_fname, expected_fname):
-    enhance_and_skullstrip_bold_wf = init_enhance_and_skullstrip_bold_wf()
-    enhance_and_skullstrip_bold_wf.inputs.inputnode.in_file = input_fname
-    res = enhance_and_skullstrip_bold_wf.run()
+    bold_reference_wf = init_bold_reference_wf(enhance_t2=True)
+    bold_reference_wf.inputs.inputnode.bold_file = input_fname
+    res = bold_reference_wf.run()
 
-    combine_masks = [node for node in res.nodes if node.name == 'combine_masks'][0]
+    combine_masks = [node for node in res.nodes if node.name.endswith('combine_masks')][0]
     overlap = symmetric_overlap(expected_fname,
                                 combine_masks.result.outputs.out_file)
 
