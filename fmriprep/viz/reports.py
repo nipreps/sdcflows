3163c3408a5b408533cf9008b098ce5468f421cb
oesteban <code@oscaresteban.es>
2018-05-11 09:37:54 -0700

    use nipype's copyfile, create hardlinks if possible

--- fmriprep/viz/reports.py
+++ fmriprep/viz/reports.py
@@ -15,19 +15,25 @@ import json
 import re
 
 import html
-import shutil
 
 import jinja2
-from niworkflows.nipype.utils.filemanip import loadcrash
+from niworkflows.nipype.utils.filemanip import loadcrash, copyfile
 from pkg_resources import resource_filename as pkgrf
 
+
 class Element(object):
+    """
+    Just a basic component of a report
+    """
     def __init__(self, name, title=None):
         self.name = name
         self.title = title
 
 
 class Reportlet(Element):
+    """
+    A reportlet has title, description and a list of graphical components
+    """
     def __init__(self, name, file_pattern=None, title=None, description=None, raw=False):
         self.name = name
         self.file_pattern = re.compile(file_pattern)
@@ -39,6 +45,9 @@ class Reportlet(Element):
 
 
 class SubReport(Element):
+    """
+    SubReports are sections within a Report
+    """
     def __init__(self, name, reportlets=None, title=''):
         self.name = name
         self.title = title
@@ -46,6 +55,129 @@ class SubReport(Element):
         if reportlets:
             self.reportlets += reportlets
         self.isnested = False
+
+
+class Report(object):
+    """
+    The full report object
+    """
+    def __init__(self, path, config, out_dir, run_uuid, out_filename='report.html'):
+        self.root = path
+        self.sections = []
+        self.errors = []
+        self.out_dir = out_dir
+        self.out_filename = out_filename
+        self.run_uuid = run_uuid
+
+        self._load_config(config)
+
+    def _load_config(self, config):
+        with open(config, 'r') as configfh:
+            config = json.load(configfh)
+
+        self.index(config['sections'])
+
+    def index(self, config):
+        fig_dir = 'figures'
+        subject_dir = self.root.split('/')[-1]
+        subject = re.search('^(?P<subject_id>sub-[a-zA-Z0-9]+)$', subject_dir).group()
+        svg_dir = os.path.join(self.out_dir, 'fmriprep', subject, fig_dir)
+        os.makedirs(svg_dir, exist_ok=True)
+
+        reportlet_list = list(sorted([str(f) for f in Path(self.root).glob('**/*.*')]))
+
+        for subrep_cfg in config:
+            reportlets = []
+            for reportlet_cfg in subrep_cfg['reportlets']:
+                rlet = Reportlet(**reportlet_cfg)
+                for src in reportlet_list:
+                    ext = src.split('.')[-1]
+                    if rlet.file_pattern.search(src):
+                        contents = None
+                        if ext == 'html':
+                            with open(src) as fp:
+                                contents = fp.read().strip()
+                        elif ext == 'svg':
+                            fbase = os.path.basename(src)
+                            copyfile(src, os.path.join(svg_dir, fbase),
+                                     copy=True, use_hardlink=True)
+                            contents = os.path.join(subject, fig_dir, fbase)
+
+                        if contents:
+                            rlet.source_files.append(src)
+                            rlet.contents.append(contents)
+
+                if rlet.source_files:
+                    reportlets.append(rlet)
+
+            if reportlets:
+                sub_report = SubReport(
+                    subrep_cfg['name'], reportlets=reportlets,
+                    title=subrep_cfg.get('title'))
+                self.sections.append(order_by_run(sub_report))
+
+        error_dir = os.path.join(self.out_dir, "fmriprep", subject, 'log', self.run_uuid)
+        if os.path.isdir(error_dir):
+            self.index_error_dir(error_dir)
+
+    def index_error_dir(self, error_dir):
+        ''' Crawl subjects crash directory for the corresponding run and return text for
+            .pklz crash file found. '''
+        for root, directories, filenames in os.walk(error_dir):
+            for f in filenames:
+                crashtype = os.path.splitext(f)[1]
+                if f[:5] == 'crash' and crashtype == '.pklz':
+                    self.errors.append(self._read_pkl(os.path.join(root, f)))
+                elif f[:5] == 'crash' and crashtype == '.txt':
+                    self.errors.append(self._read_txt(os.path.join(root, f)))
+
+    @staticmethod
+    def _read_pkl(fname):
+        crash_data = loadcrash(fname)
+        data = {'file': fname,
+                'traceback': ''.join(crash_data['traceback']).replace("\\n", "<br \>")}
+        if 'node' in crash_data:
+            data['node'] = crash_data['node']
+            if data['node'].base_dir:
+                data['node_dir'] = data['node'].output_dir()
+            else:
+                data['node_dir'] = "Node crashed before execution"
+            data['inputs'] = sorted(data['node'].inputs.trait_get().items())
+        return data
+
+    @staticmethod
+    def _read_txt(fname):
+        with open(fname, 'r') as fobj:
+            crash_data = fobj.read()
+        lines = crash_data.splitlines()
+        data = {'file': fname}
+        traceback_start = 0
+        if lines[0].startswith('Node'):
+            data['node'] = lines[0].split(': ', 1)[1]
+            data['node_dir'] = lines[1].split(': ', 1)[1]
+            inputs = []
+            for i, line in enumerate(lines[5:], 5):
+                if not line:
+                    traceback_start = i + 1
+                    break
+                inputs.append(tuple(map(html.escape, line.split(' = ', 1))))
+            data['inputs'] = sorted(inputs)
+        else:
+            data['node_dir'] = "Node crashed before execution"
+        data['traceback'] = '\n'.join(lines[traceback_start:])
+        return data
+
+    def generate_report(self):
+        searchpath = pkgrf('fmriprep', '/')
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(searchpath=searchpath),
+            trim_blocks=True, lstrip_blocks=True
+        )
+        report_tpl = env.get_template('viz/report.tpl')
+        report_render = report_tpl.render(sub_reports=self.sections, errors=self.errors)
+        with open(os.path.join(self.out_dir, "fmriprep", self.out_filename), 'w') as fp:
+            fp.write(report_render)
+        return len(self.errors)
 
 
 def order_by_run(subreport):
@@ -109,127 +241,6 @@ def generate_name_title(filename):
         run=" Run: " + outputs['run_id'] if outputs['run_id'] else ''
     )
     return name.strip('_'), title
-
-
-class Report(object):
-
-    def __init__(self, path, config, out_dir, run_uuid, out_filename='report.html'):
-        self.root = path
-        self.sub_reports = []
-        self.errors = []
-        self.out_dir = out_dir
-        self.out_filename = out_filename
-        self.run_uuid = run_uuid
-
-        self._load_config(config)
-
-    def _load_config(self, config):
-        with open(config, 'r') as configfh:
-            config = json.load(configfh)
-
-        self.index(config['sub_reports'])
-
-    def index(self, config):
-        fig_dir = 'figures'
-        subject_dir = self.root.split('/')[-1]
-        subject = re.search('^(?P<subject_id>sub-[a-zA-Z0-9]+)$', subject_dir).group()
-        svg_dir = os.path.join(self.out_dir, 'fmriprep', subject, fig_dir)
-        os.makedirs(svg_dir, exist_ok=True)
-
-        reportlet_list = list(sorted([str(f) for f in Path(self.root).glob('**/*.*')]))
-
-        for subrep_cfg in config:
-            reportlets = []
-            for reportlet_cfg in subrep_cfg['reportlets']:
-                rlet = Reportlet(**reportlet_cfg)
-                for f in reportlet_list:
-                    ext = f.split('.')[-1]
-                    if rlet.file_pattern.search(f):
-                        contents = None
-                        if ext == 'html':
-                            with open(f) as fp:
-                                contents = fp.read().strip()
-                        elif ext == 'svg':
-                            fbase = os.path.basename(f)
-                            newf = os.path.join(svg_dir, fbase)
-                            shutil.copy(f, newf)
-                            contents = os.path.join(subject, fig_dir, fbase)
-
-                        if contents:
-                            rlet.source_files.append(f)
-                            rlet.contents.append(contents)
-
-                if rlet.source_files:
-                    reportlets.append(rlet)
-
-            if reportlets:
-                sub_report = SubReport(
-                    subrep_cfg['name'], reportlets=reportlets,
-                    title=subrep_cfg.get('title'))
-                self.sub_reports.append(order_by_run(sub_report))
-
-        error_dir = os.path.join(self.out_dir, "fmriprep", subject, 'log', self.run_uuid)
-        if os.path.isdir(error_dir):
-            self.index_error_dir(error_dir)
-
-    def index_error_dir(self, error_dir):
-        ''' Crawl subjects crash directory for the corresponding run and return text for
-            .pklz crash file found. '''
-        for root, directories, filenames in os.walk(error_dir):
-            for f in filenames:
-                crashtype = os.path.splitext(f)[1]
-                if f[:5] == 'crash' and crashtype == '.pklz':
-                    self.errors.append(self._read_pkl(os.path.join(root, f)))
-                elif f[:5] == 'crash' and crashtype == '.txt':
-                    self.errors.append(self._read_txt(os.path.join(root, f)))
-
-    @staticmethod
-    def _read_pkl(fname):
-        crash_data = loadcrash(fname)
-        data = {'file': fname,
-                'traceback': ''.join(crash_data['traceback']).replace("\\n", "<br \>")}
-        if 'node' in crash_data:
-            data['node'] = crash_data['node']
-            if data['node'].base_dir:
-                data['node_dir'] = data['node'].output_dir()
-            else:
-                data['node_dir'] = "Node crashed before execution"
-            data['inputs'] = sorted(data['node'].inputs.trait_get().items())
-        return data
-
-    @staticmethod
-    def _read_txt(fname):
-        with open(fname, 'r') as fobj:
-            crash_data = fobj.read()
-        lines = crash_data.splitlines()
-        data = {'file': fname}
-        traceback_start = 0
-        if lines[0].startswith('Node'):
-            data['node'] = lines[0].split(': ', 1)[1]
-            data['node_dir'] = lines[1].split(': ', 1)[1]
-            inputs = []
-            for i, line in enumerate(lines[5:], 5):
-                if not line:
-                    traceback_start = i + 1
-                    break
-                inputs.append(tuple(map(html.escape, line.split(' = ', 1))))
-            data['inputs'] = sorted(inputs)
-        else:
-            data['node_dir'] = "Node crashed before execution"
-        data['traceback'] = '\n'.join(lines[traceback_start:])
-        return data
-
-    def generate_report(self):
-        searchpath = pkgrf('fmriprep', '/')
-        env = jinja2.Environment(
-            loader=jinja2.FileSystemLoader(searchpath=searchpath),
-            trim_blocks=True, lstrip_blocks=True
-        )
-        report_tpl = env.get_template('viz/report.tpl')
-        report_render = report_tpl.render(sub_reports=self.sub_reports, errors=self.errors)
-        with open(os.path.join(self.out_dir, "fmriprep", self.out_filename), 'w') as fp:
-            fp.write(report_render)
-        return len(self.errors)
 
 
 def run_reports(reportlets_dir, out_dir, subject_label, run_uuid):
