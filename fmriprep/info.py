f8680733cc15a1673449e582c7f11c475e3c505f
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-07-05 12:22:41 -0400

    PIN: niworkflows 0.4.2

--- fmriprep/info.py
+++ fmriprep/info.py
@@ -71,7 +71,7 @@ REQUIRES = [
     'pybids>=0.5.1',
     'nitime',
     'nipype>=1.1.0',
-    'niworkflows>=0.4.1',
+    'niworkflows>=0.4.2',
     'statsmodels',
     'seaborn',
     'indexed_gzip>=0.8.2',
@@ -80,8 +80,6 @@ REQUIRES = [
 ]
 
 LINKS_REQUIRES = [
-    'git+https://github.com/effigies/niworkflows.git@'
-    '3a77927041ceed82b6ae20dcd8cbf535f722a6f4#egg=niworkflows',
 ]
 
 TESTS_REQUIRES = [
