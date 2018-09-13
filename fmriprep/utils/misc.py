dab9cce4e3ede1a10387731f8fbbab973dd7cd4e
Elizabeth DuPre <emd222@cornell.edu>
2018-09-13 10:19:38 -0400

    Update namesource for meepi

--- fmriprep/utils/misc.py
+++ fmriprep/utils/misc.py
@@ -24,6 +24,26 @@ def fix_multi_T1w_source_name(in_files):
     return os.path.join(base, "sub-%s_T1w.nii.gz" % subject_label)
 
 
+def meepi_optimal_comb_source_name(in_files):
+    """
+    Create a new source name when optimally
+    combining multiple multi-echo EPIs
+
+    >>> meepi_optimal_comb_source_name([
+    ...     'sub-01_run-01_echo-1_bold.nii.gz',
+    ...     'sub-01_run-01_echo-2_bold.nii.gz',
+    ...     'sub-01_run-01_echo-3_bold.nii.gz',])
+    sub-01_run-01_bold.nii.gz'
+
+    """
+    import os
+    from nipype.utils.filemanip import filename_to_list
+    base, in_file = os.path.split(filename_to_list(in_files)[0])
+    keys_pre_echo = in_file.split("_")[:-2]
+    file_label = "_".join(keys_pre_echo)
+    return os.path.join(base, "%s_bold.nii.gz" % file_label)
+
+
 def add_suffix(in_files, suffix):
     """
     Wrap nipype's fname_presuffix to conveniently just add a prefix
