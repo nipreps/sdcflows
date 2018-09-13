16f2bbb58d75ec3367cb9831508c7136b4f92fe3
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2018-09-13 16:04:53 -0400

    Replace MCFLIRT with 3dVolReg

--- fmriprep/utils/misc.py
+++ fmriprep/utils/misc.py
@@ -7,6 +7,58 @@ Miscellaneous utilities
 """
 
 
+def split_and_deoblique_func(in_file):
+    import os
+    from nilearn.image import iter_img
+    import nibabel as nb
+    import numpy as np
+    out_files = []
+    for i, img in enumerate(iter_img(in_file)):
+        out_file = os.path.abspath('vol%04d.nii.gz'%i)
+        affine = img.affine
+        affine[:3,:3] = np.diag(np.diag(affine[:3, :3]))
+        nb.Nifti1Image(np.asanyarray(img.dataobj), affine, img.header).to_filename(out_file)
+        out_files.append(out_file)
+    return out_files
+
+
+def afni2itk_func(in_file):
+    import os
+    from scipy.io import loadmat, savemat
+    from numpy import loadtxt, around, hstack, vstack, zeros, float64
+
+    def read_afni_affine(input_file, debug=False):
+        orig_afni_mat = loadtxt(input_file)
+        if debug:
+            print(orig_afni_mat)
+
+        output = []
+        for i in range(orig_afni_mat.shape[0]):
+            output.append(vstack((orig_afni_mat[i,:].reshape(3, 4, order='C'), [0, 0, 0, 1])))
+        return output
+
+    def get_ants_dict(affine, debug=False):
+        out_dict = {}
+        out_dict['AffineTransform_double_3_3'] = hstack(
+            (affine[:3, :3].reshape(1, -1), affine[:3, 3].reshape(1, -1))).reshape(-1, 1).astype(float64)
+        out_dict['fixed'] = zeros((3, 1))
+        if debug:
+            print(out_dict)
+
+        return out_dict
+
+    out_file = os.path.abspath('mc4d.txt')
+    with open(out_file, 'w') as fp:
+        fp.write("#Insight Transform File V1.0\n")
+        for i, affine in enumerate(read_afni_affine(in_file)):
+            fp.write("#Transform %d\n"%i)
+            fp.write("Transform: AffineTransform_double_3_3\n")
+            trans_dict = get_ants_dict(affine)
+            fp.write("Parameters: " + ' '.join(["%g"%i for i in list(trans_dict['AffineTransform_double_3_3'])]) + "\n")
+            fp.write("FixedParameters: " + ' '.join(["%g"%i for i in list(trans_dict['fixed'])]) + "\n")
+    return out_file
+
+
 def fix_multi_T1w_source_name(in_files):
     """
     Make up a generic source name when there are multiple T1s
