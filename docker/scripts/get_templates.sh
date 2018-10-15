cc03d5e695340e65a34373d4edea5cd39e6a91d0
oesteban <code@oscaresteban.es>
2018-10-15 11:43:42 -0700

    [ENH] Template-based masking of EPI boldrefs

--- docker/scripts/get_templates.sh
+++ docker/scripts/get_templates.sh
@@ -6,10 +6,12 @@ ASYM_09C_TEMPLATE="https://files.osf.io/v1/resources/fvuh8/providers/osfstorage/
 ASYM_09C_SHA256="a24699ba0d13f72d0f8934cc211cb80bfd9c9a077b481d9b64295cf5275235a9"
 OASIS_TEMPLATE="https://files.osf.io/v1/resources/fvuh8/providers/osfstorage/584123a29ad5a1020913609d"
 OASIS_SHA256="d87300e91346c16f55baf6f54f5f990bc020b61e8d5df9bcc3abb0cc4b943113"
-NKI_TEMPLATE="https://files.osf.io/v1/resources/fvuh8/providers/osfstorage/59cd90f46c613b02b3d79782"
-NKI_SHA256="4bba067f6675d15be96b205cb227e18a540673fd7e4577e13feedcef3a6f0ec5"
+NKI_TEMPLATE="https://files.osf.io/v1/resources/fvuh8/providers/osfstorage/5bc3fad82aa873001bc5a553"
+NKI_SHA256="9c08713d067bcf13baa61b01a9495e526b55d1f148d951da01e082679f076fa9"
 OASIS_DKT31_TEMPLATE="https://files.osf.io/v1/resources/fvuh8/providers/osfstorage/5b16f17aeca4a80012bd7542"
 OASIS_DKT31_SHA256="623fa7141712b1a7263331dba16eb069a4443e9640f52556c89d461611478145"
+EPI_TEMPLATE="https://files.osf.io/v1/resources/fvuh8/providers/osfstorage/5bc12155ac011000176bff82"
+EPI_SHA256="fcd6980ef98c9d7622c6dc2a7747ff51ba3909d98e2a740df9a8265d50920d1b"
 
 GET(){
     URL=$1; SHA256=$2;
@@ -36,4 +38,6 @@ echo "Getting NKI template"
 GET "$NKI_TEMPLATE" "$NKI_SHA256"
 echo "Getting OASIS DKT31 template"
 GET "$OASIS_DKT31_TEMPLATE" "$OASIS_DKT31_SHA256"
+echo "Getting fMRIPrep's BOLDref template"
+GET "$EPI_TEMPLATE" "$EPI_SHA256"
 echo "Done!"
