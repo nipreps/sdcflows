bee3cd3796f0e2aa5009ea83483e937e570c3240
oesteban <code@oscaresteban.es>
2018-12-10 12:58:54 -0800

    revise ``__init__.py`` files

--- fmriprep/viz/__init__.py
+++ fmriprep/viz/__init__.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
 The fmriprep reporting engine for visual assessment
 """
 from .reports import run_reports, generate_reports
+
+__all__ = ['run_reports', 'generate_reports']
