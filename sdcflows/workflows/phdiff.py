be442c3298d1590020ef0961094437de310d9403
Matt Cieslak <mattcieslak@gmail.com>
2019-08-17 00:11:17 -0400

    start workflow for calculating phasediff from phase images

--- sdcflows/workflows/phdiff.py
+++ sdcflows/workflows/phdiff.py
@@ -19,12 +19,12 @@ This corresponds to `this section of the BIDS specification
 
 from nipype.interfaces import fsl, afni, utility as niu
 from nipype.pipeline import engine as pe
-from niflow.nipype1.workflows.dmri.fsl.utils import (
-    siemens2rads, demean_image, cleanup_edge_pipeline)
+from niflow.nipype1.workflows.dmri.fsl.utils import siemens2rads
 from niworkflows.engine.workflows import LiterateWorkflow as Workflow
 
 from ..interfaces.fmap import Phasediff2Fieldmap, ProcessPhases
 from .b0 import init_prepare_magnitude_wf, init_fmap_postproc_wf
+
 
 def init_calculate_phasediff_wf(omp_nthreads, name='create_phasediff_wf'):
     """
