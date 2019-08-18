2a5facfde5ee3800099df4f2eec541d1f0b494fa
Matt Cieslak <mattcieslak@gmail.com>
2019-08-18 16:45:30 -0400

    adopted fsl's subtraction for making a phasediff

--- sdcflows/workflows/phdiff.py
+++ sdcflows/workflows/phdiff.py
@@ -28,7 +28,8 @@ from niworkflows.interfaces.masks import BETRPT
 from ..interfaces.fmap import Phasediff2Fieldmap, ProcessPhases
 
 
-def init_calculate_phasediff_wf(omp_nthreads, difference='arctan', name='create_phasediff_fw'):
+def init_calculate_phasediff_wf(omp_nthreads, difference='unwrapped_subtraction',
+                                name='create_phasediff_wf'):
     """
     Create a phasediff image from two phase images.
 
@@ -55,9 +56,15 @@ def init_calculate_phasediff_wf(omp_nthreads, difference='arctan', name='create_
             Metadata dictionary corresponding to the phase1 input
         phase2_metadata : dict
             Metadata dictionary corresponding to the phase2 input
+        magnitude : pathlike
+            Preprocessed magnitude image
+        mask_file : pathlike
+            Brain mask image
     """
     inputnode = pe.Node(
-        niu.IdentityInterface(fields=['phase1', 'phase2', 'phase1_metadata', 'phase2_metadata']),
+        niu.IdentityInterface(
+            fields=['phase1', 'phase2', 'phase1_metadata', 'phase2_metadata', 'magnitude',
+                    'mask_file']),
         name='inputnode')
 
     outputnode = pe.Node(niu.IdentityInterface(
@@ -67,26 +74,21 @@ def init_calculate_phasediff_wf(omp_nthreads, difference='arctan', name='create_
 
     # Rescale images and calculate phasediff with arctan
     process_phases = pe.Node(ProcessPhases(), name='process_phases')
-
-    if difference == 'arctan':
-        workflow.connect([
-            (process_phases, outputnode, [('phasediff', 'phasediff')])
-        ])
-    else:
-        unwrap_phase1 = pe.Node(fsl.PRELUDE(), name='unwrap_phase1')
-        unwrap_phase2 = pe.Node(fsl.PRELUDE(), name='unwrap_phase2')
-        subtract_phases = pe.Node(
-            afni.Calc(outputtype='NIFTI_GZ', expr='b-a'), name='subtract_phases')
-
-        workflow.connect([
-            (process_phases, unwrap_phase1, [('short_te_phase_image', 'phase_file')]),
-            (process_phases, unwrap_phase2, [('long_te_phase_image', 'phase_file')]),
-            (unwrap_phase1, subtract_phases, [('unwrapped_phase_file', 'in_file_a')]),
-            (unwrap_phase2, subtract_phases, [('unwrapped_phase_file', 'in_file_b')]),
-            (subtract_phases, outputnode, [('out_file', 'phasediff')])
-        ])
+    unwrap_phase1 = pe.Node(fsl.PRELUDE(), name='unwrap_phase1')
+    unwrap_phase2 = pe.Node(fsl.PRELUDE(), name='unwrap_phase2')
+    subtract_phases = pe.Node(
+        afni.Calc(outputtype='NIFTI_GZ', expr='b-a'), name='subtract_phases')
 
     workflow.connect([
+        (inputnode, unwrap_phase1, [('magnitude', 'magnitude_file')]),
+        (inputnode, unwrap_phase2, [('magnitude', 'magnitude_file')]),
+        (inputnode, unwrap_phase1, [('mask_file', 'mask_file')]),
+        (inputnode, unwrap_phase2, [('mask_file', 'mask_file')]),
+        (process_phases, unwrap_phase1, [('short_te_phase_image', 'phase_file')]),
+        (process_phases, unwrap_phase2, [('long_te_phase_image', 'phase_file')]),
+        (unwrap_phase1, subtract_phases, [('unwrapped_phase_file', 'in_file_a')]),
+        (unwrap_phase2, subtract_phases, [('unwrapped_phase_file', 'in_file_b')]),
+        (subtract_phases, outputnode, [('out_file', 'phasediff')]),
         (inputnode, process_phases, [
             ('phase1', 'phase1_file'),
             ('phase2', 'phase2_file'),
@@ -98,7 +100,7 @@ def init_calculate_phasediff_wf(omp_nthreads, difference='arctan', name='create_
     return workflow
 
 
-def init_phdiff_wf(omp_nthreads, name='phdiff_wf'):
+def init_phdiff_wf(omp_nthreads, create_phasediff=False, name='phdiff_wf'):
     """
     Distortion correction of EPI sequences using phase-difference maps.
 
@@ -128,6 +130,14 @@ def init_phdiff_wf(omp_nthreads, name='phdiff_wf'):
         Path to the corresponding phase-difference file.
     metadata : dict
         Metadata dictionary corresponding to the phasediff input
+    phase1 : pathlike
+        Path to one of the phase images.
+    phase2 : pathlike
+        Path to the another phase image.
+    phase1_metadata : dict
+        Metadata dictionary corresponding to the phase1 input
+    phase2_metadata : dict
+        Metadata dictionary corresponding to the phase2 input
 
     Outputs
     -------
@@ -148,8 +158,11 @@ using a custom workflow of *fMRIPrep* derived from D. Greve's `epidewarp.fsl`
 further improvements of HCP Pipelines [@hcppipelines].
 """
 
-    inputnode = pe.Node(niu.IdentityInterface(fields=['magnitude', 'phasediff', 'metadata']),
-                        name='inputnode')
+    inputnode = pe.Node(
+        niu.IdentityInterface(
+            fields=['magnitude', 'phasediff', 'metadata', 'phase1', 'phase2', 'phase1_metadata',
+                    'phase2_metadata']),
+        name='inputnode')
 
     outputnode = pe.Node(niu.IdentityInterface(
         fields=['fmap', 'fmap_ref', 'fmap_mask']), name='outputnode')
@@ -167,11 +180,41 @@ further improvements of HCP Pipelines [@hcppipelines].
     # dilate = pe.Node(fsl.maths.MathsCommand(
     #     nan2zeros=True, args='-kernel sphere 5 -dilM'), name='MskDilate')
 
-    # phase diff -> radians
-    pha2rads = pe.Node(niu.Function(function=siemens2rads), name='pha2rads')
+    preprocessed_phasediff = pe.Node(
+        niu.IdentityInterface(fields=['phasediff', 'phasediff_metadata']),
+        name='preprocessed_phasediff')
 
-    # FSL PRELUDE will perform phase-unwrapping
-    prelude = pe.Node(fsl.PRELUDE(), name='prelude')
+    if create_phasediff:
+        calc_phdiff_wf = init_calculate_phasediff_wf(omp_nthreads=omp_nthreads)
+        workflow.connect([
+            (bet, calc_phdiff_wf, [('mask_file', 'inputnode.mask_file')]),
+            (n4, calc_phdiff_wf, [('output_image', 'inputnode.magnitude')]),
+            (inputnode, calc_phdiff_wf, [
+                ('phase1', 'inputnode.phase1'),
+                ('phase2', 'inputnode.phase2'),
+                ('phase1_metadata', 'inputnode.phase1_metadata'),
+                ('phase2_metadata', 'inputnode.phase2_metadata')]),
+            (calc_phdiff_wf, preprocessed_phasediff, [
+                ('outputnode.phasediff', 'phasediff'),
+                ('outputnode.phasediff_metadata', 'phasediff_metadata')])
+        ])
+
+    else:
+
+        # FSL PRELUDE will perform phase-unwrapping
+        prelude = pe.Node(fsl.PRELUDE(), name='prelude')
+
+        # phase diff -> radians
+        pha2rads = pe.Node(niu.Function(function=siemens2rads), name='pha2rads')
+
+        workflow.connect([
+            (n4, prelude, [('output_image', 'magnitude_file')]),
+            (bet, prelude, [('mask_file', 'mask_file')]),
+            (inputnode, pha2rads, [('phasediff', 'in_file')]),
+            (pha2rads, prelude, [('out', 'phase_file')]),
+            (prelude, preprocessed_phasediff, [('unwrapped_phase_file', 'phasediff')]),
+            (inputnode, preprocessed_phasediff, [('metadata', 'phasediff_metadata')]),
+        ])
 
     denoise = pe.Node(fsl.SpatialFilter(operation='median', kernel_shape='sphere',
                                         kernel_size=3), name='denoise')
@@ -188,19 +231,15 @@ further improvements of HCP Pipelines [@hcppipelines].
     # rsec2hz (divide by 2pi)
 
     workflow.connect([
-        (inputnode, compfmap, [('metadata', 'metadata')]),
         (inputnode, magmrg, [('magnitude', 'in_files')]),
         (magmrg, n4, [('out_avg', 'input_image')]),
-        (n4, prelude, [('output_image', 'magnitude_file')]),
         (n4, bet, [('output_image', 'in_file')]),
-        (bet, prelude, [('mask_file', 'mask_file')]),
-        (inputnode, pha2rads, [('phasediff', 'in_file')]),
-        (pha2rads, prelude, [('out', 'phase_file')]),
-        (prelude, denoise, [('unwrapped_phase_file', 'in_file')]),
+        (preprocessed_phasediff, denoise, [('phasediff', 'in_file')]),
         (denoise, demean, [('out_file', 'in_file')]),
         (demean, cleanup_wf, [('out', 'inputnode.in_file')]),
         (bet, cleanup_wf, [('mask_file', 'inputnode.in_mask')]),
         (cleanup_wf, compfmap, [('outputnode.out_file', 'in_file')]),
+        (preprocessed_phasediff, compfmap, [('phasediff_metadata', 'metadata')]),
         (compfmap, outputnode, [('out_file', 'fmap')]),
         (bet, outputnode, [('mask_file', 'fmap_mask'),
                            ('out_file', 'fmap_ref')]),
--- sdcflows/workflows/tests/test_phdiff.py
+++ sdcflows/workflows/tests/test_phdiff.py
@@ -52,3 +52,48 @@ def test_workflow(bids_layouts, tmpdir, output_path, dataset, workdir):
         wf.base_dir = str(workdir)
 
     wf.run()
+
+
+def test_phases_workflow(bids_layouts, tmpdir, output_path):
+    """Test creation of the workflow."""
+    tmpdir.chdir()
+
+    data = bids_layouts['ds001600']
+    wf = Workflow(name='tstphasesworkflow')
+    phdiff_wf = init_phdiff_wf(omp_nthreads=1, create_phasediff=True)
+    phdiff_wf.inputs.inputnode.magnitude = data.get(
+        suffix=['magnitude1', 'magnitude2'],
+        acq='v2',
+        return_type='file',
+        extension=['.nii', '.nii.gz'])
+
+    phase1_file = data.get(suffix='phase1', acquisition='v2',
+                           extension=['.nii', '.nii.gz'])[0]
+    phase2_file = data.get(suffix='phase2', acquisition='v2',
+                           extension=['.nii', '.nii.gz'])[0]
+
+    phdiff_wf.inputs.inputnode.phase1 = phase1_file.path
+    phdiff_wf.inputs.inputnode.phase2 = phase2_file.path
+    phdiff_wf.inputs.inputnode.phase1_metadata = phase1_file.get_metadata()
+    phdiff_wf.inputs.inputnode.phase2_metadata = phase2_file.get_metadata()
+
+    if output_path:
+        from ...interfaces.reportlets import FieldmapReportlet
+        rep = pe.Node(FieldmapReportlet(), 'simple_report')
+
+        dsink = pe.Node(DerivativesDataSink(
+            base_directory=str(output_path), keep_dtype=True), name='dsink')
+        dsink.interface.out_path_base = 'sdcflows'
+        dsink.inputs.source_file = phase1_file.path
+
+        wf.connect([
+            (phdiff_wf, rep, [
+                ('outputnode.fmap', 'fieldmap'),
+                ('outputnode.fmap_ref', 'reference'),
+                ('outputnode.fmap_mask', 'mask')]),
+            (rep, dsink, [('out_report', 'in_file')]),
+        ])
+    else:
+        wf.add_nodes([phdiff_wf])
+
+    wf.run()
