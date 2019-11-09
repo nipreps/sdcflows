a32778f70d05e775d289870827697f2149763c9b
Matt Cieslak <mattcieslak@gmail.com>
2019-11-09 08:27:24 -0500

    Add rebased master branch [fresh workdir]

--- sdcflows/workflows/gre.py
+++ sdcflows/workflows/gre.py
@@ -1,13 +1,13 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""Processing phase-difference (aka :abbr:`GRE (gradient recalled echo)`) fieldmaps.
+"""Processing phase-difference (aka :abbr:`GRE (gradient-recalled echo)`) fieldmaps.
 
 .. _gre-fieldmaps:
 
 Workflows for processing :abbr:`GRE (gradient recalled echo)` fieldmaps
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Workflows for preparing the magnitude part of :abbr:`GRE (gradient recalled echo)` fieldmap
+Workflows for preparing the magnitude part of :abbr:`GRE (gradient-recalled echo)` fieldmap
 images and cleaning up the fieldmaps created from the phases or phasediff.
 
 """
@@ -23,7 +23,7 @@ from ..interfaces.fmap import FieldEnhance
 
 def init_prepare_magnitude_wf(omp_nthreads, name='magnitude_wf'):
     """
-    Prepare the magnitude part of :abbr:`GRE (gradient recalled echo)` fieldmaps.
+    Prepare the magnitude part of :abbr:`GRE (gradient-recalled echo)` fieldmaps.
 
     Average (if not done already) the magnitude part of the
     :abbr:`GRE (gradient recalled echo)` images, run N4 to
