3cfc3eac906e3dbe3c1373898689a2ae722c7770
Oscar Esteban <code@oscaresteban.es>
2020-10-29 09:37:32 +0100

    enh: add a phase-difference model submodule

---
+++ sdcflows/models/fieldmap.py
@@ -0,0 +1,366 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Processing phase-difference and *directly measured* B0 maps.
+
+.. _sdc_phasediff :
+
+Phase-difference B0 estimation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The inhomogeneity of the :math:`B_0` field inside the scanner at each voxel is
+proportional to the phase drift (at that voxel) between two subsequent
+:abbr:`GRE (gradient-recalled echo)` acquisitions.
+
+This corresponds to `this section of the BIDS specification
+<https://bids-specification.readthedocs.io/en/stable/04-modality-specific-files/01-magnetic-resonance-imaging-data.html#two-phase-images-and-two-magnitude-images>`__.
+Some scanners produce one ``phasediff`` map, where the drift between the two echos has
+already been calulated (see `the corresponding section of BIDS
+<https://bids-specification.readthedocs.io/en/stable/04-modality-specific-files/01-magnetic-resonance-imaging-data.html#case-1-phase-difference-map-and-at-least-one-magnitude-image>__`).
+
+.. _sdc_direct_b0 :
+
+Direct B0 mapping sequences
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The inhomogeneity of the :math:`B_0` field can directly be mapped with a
+some MR schemes (such as :abbr:`SE (spiral echo)`).
+These *fieldmaps* are described with more detail `here
+<https://cni.stanford.edu/wiki/GE_Processing#Fieldmaps>`__.
+
+This corresponds to `this section of the BIDS specification
+<https://bids-specification.readthedocs.io/en/stable/04-modality-specific-files/01-magnetic-resonance-imaging-data.html#case-3-direct-field-mapping>`__.
+
+
+"""
+
+from nipype.pipeline import engine as pe
+from nipype.interfaces import utility as niu
+from niworkflows.engine.workflows import LiterateWorkflow as Workflow
+from niworkflows.interfaces.images import IntraModalMerge
+
+
+def init_fmap_wf(omp_nthreads, mode="phase-diff", name="fmap_wf"):
+    """
+    Estimate the fieldmap based on a field-mapping MRI acquisition.
+
+    Estimates the fieldmap using either one phase-difference map or
+    image and one or more
+    magnitude images corresponding to two or more :abbr:`GRE (Gradient Echo sequence)`
+    acquisitions.
+
+    When we have a sequence that directly measures the fieldmap,
+    we just need to mask it (using the corresponding magnitude image)
+    to remove the noise in the surrounding air region, and ensure that
+    units are Hz.
+
+    Workflow Graph
+        .. workflow ::
+            :graph2use: orig
+            :simple_form: yes
+
+            from sdcflows.workflows.fmap import init_fmap_wf
+            wf = init_fmap_wf(omp_nthreads=6)
+
+    Parameters
+    ----------
+    omp_nthreads : int
+        Maximum number of threads an individual process may use.
+    name : str
+        Unique name of this workflow.
+
+    Inputs
+    ------
+    magnitude : str
+        Path to the corresponding magnitude image for anatomical reference.
+    fieldmap : str
+        Path to the fieldmap acquisition (``*_fieldmap.nii[.gz]`` of BIDS).
+
+    Outputs
+    -------
+    fmap : str
+        Path to the estimated fieldmap.
+    fmap_ref : str
+        Path to a preprocessed magnitude image reference.
+    fmap_mask : str
+        Path to a binary brain mask corresponding to the ``fmap`` and ``fmap_ref``
+        pair.
+
+    """
+    workflow = Workflow(name=name)
+
+    inputnode = pe.Node(
+        niu.IdentityInterface(fields=["magnitude", "fieldmap"]), name="inputnode"
+    )
+    outputnode = pe.Node(
+        niu.IdentityInterface(fields=["fmap", "fmap_ref", "fmap_mask"]),
+        name="outputnode",
+    )
+
+    magnitude_wf = init_magnitude_wf(omp_nthreads=omp_nthreads)
+    fmap_postproc_wf = init_fmap_postproc_wf(omp_nthreads=omp_nthreads)
+
+    # fmt: off
+    workflow.connect([
+        (inputnode, magnitude_wf, [("magnitude", "inputnode.magnitude")]),
+        (magnitude_wf, fmap_postproc_wf, [
+            ("outputnode.fmap_mask", "inputnode.fmap_mask"),
+            ("outputnode.fmap_ref", "inputnode.fmap_ref")]),
+        (magnitude_wf, outputnode, [
+            ("outputnode.fmap_mask", "fmap_mask"),
+            ("outputnode.fmap_ref", "fmap_ref"),
+        ]),
+        (fmap_postproc_wf, outputnode, [("outputnode.out_fmap", "fmap")]),
+    ])
+    # fmt: on
+
+    if mode == "phase-diff":
+        from .phdiff import init_phdiff_wf
+
+        workflow.__desc__ = """\
+A *B<sub>0</sub>* nonuniformity map (or *fieldmap*) was estimated from the
+phase-drift map(s) measure with two consecutive GRE (gradient-recall echo)
+acquisitions.
+"""
+        phdiff_wf = init_phdiff_wf(omp_nthreads)
+
+        # fmt: off
+        workflow.connect([
+            (inputnode, phdiff_wf, [("fieldmap", "inputnode.magnitude")]),
+            (magnitude_wf, phdiff_wf, [
+                ("outputnode.fmap_ref", "inputnode.magnitude"),
+                ("outputnode.fmap_mask", "inputnode.mask"),
+            ]),
+            (phdiff_wf, fmap_postproc_wf, [
+                ("outputnode.fieldmap", "inputnode.fmap"),
+            ]),
+        ])
+        # fmt: on
+    else:
+        from niworkflows.interfaces.nibabel import ApplyMask
+        workflow.__desc__ = """\
+A *B<sub>0</sub>* nonuniformity map (or *fieldmap*) was directly measured with
+an MRI scheme designed with that purpose (e.g., a spiral pulse sequence).
+"""
+        # Merge input fieldmap images
+        fmapmrg = pe.Node(IntraModalMerge(zero_based_avg=False, hmc=False), name="fmapmrg")
+        applymsk = pe.Node(ApplyMask(), name="applymsk")
+        # fmt: off
+        workflow.connect([
+            (inputnode, fmapmrg, [("fieldmap", "in_files")]),
+            (fmapmrg, applymsk, [("out_avg", "in_file")]),
+            (magnitude_wf, applymsk, [("outputnode.fmap_mask", "in_mask")]),
+            (applymsk, fmap_postproc_wf, [("out_file", "inputnode.fmap")]),
+        ])
+        # fmt: on
+
+    return workflow
+
+
+def init_magnitude_wf(omp_nthreads, name="magnitude_wf"):
+    """
+    Prepare the magnitude part of :abbr:`GRE (gradient-recalled echo)` fieldmaps.
+
+    Average (if not done already) the magnitude part of the
+    :abbr:`GRE (gradient recalled echo)` images, run N4 to
+    correct for B1 field nonuniformity, and skull-strip the
+    preprocessed magnitude.
+
+    Workflow Graph
+        .. workflow ::
+            :graph2use: orig
+            :simple_form: yes
+
+            from sdcflows.workflows.fmap import init_magnitude_wf
+            wf = init_magnitude_wf(omp_nthreads=6)
+
+    Parameters
+    ----------
+    omp_nthreads : int
+        Maximum number of threads an individual process may use
+    name : str
+        Name of workflow (default: ``prepare_magnitude_w``)
+
+    Inputs
+    ------
+    magnitude : pathlike
+        Path to the corresponding magnitude path(s).
+
+    Outputs
+    -------
+    fmap_ref : pathlike
+        Path to the fieldmap reference calculated in this workflow.
+    fmap_mask : pathlike
+        Path to a binary brain mask corresponding to the reference above.
+
+    """
+    from nipype.interfaces.ants import N4BiasFieldCorrection
+    from niworkflows.interfaces.masks import BETRPT
+
+    workflow = Workflow(name=name)
+    inputnode = pe.Node(niu.IdentityInterface(fields=["magnitude"]), name="inputnode")
+    outputnode = pe.Node(
+        niu.IdentityInterface(fields=["fmap_ref", "fmap_mask", "mask_report"]),
+        name="outputnode",
+    )
+
+    # Merge input magnitude images
+    # Do not reorient to RAS to preserve the validity of PhaseEncodingDirection
+    magmrg = pe.Node(IntraModalMerge(hmc=False, to_ras=False), name="magmrg")
+
+    # de-gradient the fields ("bias/illumination artifact")
+    n4_correct = pe.Node(
+        N4BiasFieldCorrection(dimension=3, copy_header=True),
+        name="n4_correct",
+        n_procs=omp_nthreads,
+    )
+    bet = pe.Node(BETRPT(generate_report=True, frac=0.6, mask=True), name="bet")
+
+    # fmt: off
+    workflow.connect([
+        (inputnode, magmrg, [("magnitude", "in_files")]),
+        (magmrg, n4_correct, [("out_avg", "input_image")]),
+        (n4_correct, bet, [("output_image", "in_file")]),
+        (bet, outputnode, [("mask_file", "fmap_mask"),
+                           ("out_file", "fmap_ref"),
+                           ("out_report", "mask_report")]),
+    ])
+    # fmt: on
+    return workflow
+
+
+def init_fmap_postproc_wf(
+    omp_nthreads, median_kernel_size=5, name="fmap_postproc_wf"
+):
+    """
+    Postprocess a B0 map estimated elsewhere.
+
+    This workflow denoises (mostly via smoothing) a B0 fieldmap.
+
+    Workflow Graph
+        .. workflow ::
+            :graph2use: orig
+            :simple_form: yes
+
+            from sdcflows.workflows.fmap import init_fmap_postproc_wf
+            wf = init_fmap_postproc_wf(omp_nthreads=6)
+
+    Parameters
+    ----------
+    omp_nthreads : int
+        Maximum number of threads an individual process may use
+    median_kernel_size : int
+        Size of the kernel when smoothing is done with a median filter.
+    name : str
+        Name of workflow (default: ``fmap_postproc_wf``)
+
+    Inputs
+    ------
+    fmap_mask : pathlike
+        A brain binary mask corresponding to this fieldmap.
+    fmap_ref : pathlike
+        A preprocessed magnitude/reference image for the fieldmap.
+    fmap : pathlike
+        A B0-field nonuniformity map (aka fieldmap) estimated elsewhere.
+
+    Outputs
+    -------
+    out_fmap : pathlike
+        Postprocessed fieldmap.
+
+    """
+    from nipype.interfaces.fsl import SpatialFilter
+    from niflow.nipype1.workflows.dmri.fsl.utils import cleanup_edge_pipeline
+    workflow = Workflow(name=name)
+    inputnode = pe.Node(
+        niu.IdentityInterface(fields=["fmap_mask", "fmap_ref", "fmap", "metadata"]),
+        name="inputnode",
+    )
+    outputnode = pe.Node(
+        niu.IdentityInterface(fields=["out_fmap", "metadata"]), name="outputnode"
+    )
+    recenter = pe.Node(
+        niu.Function(function=_recenter),
+        name="recenter",
+        run_without_submitting=True,
+    )
+    denoise = pe.Node(
+        SpatialFilter(
+            operation="median",
+            kernel_shape="sphere",
+            kernel_size=median_kernel_size,
+        ),
+        name="denoise",
+    )
+    demean = pe.Node(niu.Function(function=_demean), name="demean")
+    cleanup_wf = cleanup_edge_pipeline(name="cleanup_wf")
+
+    # fmt: off
+    workflow.connect([
+        (inputnode, cleanup_wf, [("fmap_mask", "inputnode.in_mask")]),
+        (inputnode, recenter, [(("fmap", _pop), "in_file")]),
+        (recenter, denoise, [("out", "in_file")]),
+        (denoise, demean, [("out_file", "in_file")]),
+        (demean, cleanup_wf, [("out", "inputnode.in_file")]),
+        (cleanup_wf, outputnode, [("outputnode.out_file", "out_fmap")]),
+        (inputnode, outputnode, [(("metadata", _pop), "metadata")]),
+    ])
+    # fmt: on
+    return workflow
+
+
+def _recenter(in_file):
+    """Recenter the phase-map distribution to the -pi..pi range."""
+    from os import getcwd
+    import numpy as np
+    import nibabel as nb
+    from nipype.utils.filemanip import fname_presuffix
+
+    nii = nb.load(in_file)
+    data = nii.get_fdata(dtype="float32")
+    msk = data != 0
+    msk[data == 0] = False
+    data[msk] -= np.median(data[msk])
+
+    out_file = fname_presuffix(in_file, suffix="_recentered", newpath=getcwd())
+    nb.Nifti1Image(data, nii.affine, nii.header).to_filename(out_file)
+    return out_file
+
+
+def _demean(in_file, in_mask=None, usemode=True):
+    """
+    Subtract the median (since it is robuster than the mean) from a map.
+
+    Parameters
+    ----------
+    usemode : bool
+        Use the mode instead of the median (should be even more robust
+        against outliers).
+
+    """
+    from os import getcwd
+    import numpy as np
+    import nibabel as nb
+    from nipype.utils.filemanip import fname_presuffix
+
+    nii = nb.load(in_file)
+    data = nii.get_fdata(dtype="float32")
+
+    msk = np.ones_like(data, dtype=bool)
+    if in_mask is not None:
+        msk[nb.load(in_mask).get_fdata(dtype="float32") < 1e-4] = False
+
+    if usemode:
+        from scipy.stats import mode
+
+        data[msk] -= mode(data[msk], axis=None)[0][0]
+    else:
+        data[msk] -= np.median(data[msk], axis=None)
+
+    out_file = fname_presuffix(in_file, suffix="_demean", newpath=getcwd())
+    nb.Nifti1Image(data, nii.affine, nii.header).to_filename(out_file)
+    return out_file
+
+
+def _pop(inlist):
+    if isinstance(inlist, (tuple, list)):
+        return inlist[0]
+    return inlist
