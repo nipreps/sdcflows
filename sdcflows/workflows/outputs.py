a06860cad083a1216b31164a150f345c1b3a8f66
Oscar Esteban <code@oscaresteban.es>
2020-11-19 21:58:34 +0100

    enh: continue refactoring

--- sdcflows/workflows/outputs.py
+++ sdcflows/workflows/outputs.py
@@ -6,7 +6,105 @@ from nipype.interfaces import utility as niu
 from niworkflows.interfaces.bids import DerivativesDataSink
 
 
-def init_sdc_unwarp_report_wf(name='sdc_unwarp_report_wf', forcedsyn=False):
+def init_fmap_derivatives_wf(
+    *,
+    bids_root,
+    output_dir,
+    write_coeff=False,
+    name="fmap_derivatives_wf",
+):
+    """
+    Set up datasinks to store derivatives in the right location.
+
+    Parameters
+    ----------
+    bids_root : :obj:`str`
+        Root path of BIDS dataset
+    output_dir : :obj:`str`
+        Directory in which to save derivatives
+    name : :obj:`str`
+        Workflow name (default: ``"fmap_derivatives_wf"``)
+
+    Inputs
+    ------
+    source_file
+        A fieldmap file of the BIDS dataset that will serve for naming reference.
+    fieldmap
+        The preprocessed fieldmap, in its original space with Hz units.
+    fmap_coeff
+        Field coefficient(s) file(s)
+    fmap_ref
+        An anatomical reference (e.g., magnitude file)
+
+    """
+    workflow = pe.Workflow(name=name)
+
+    inputnode = pe.Node(
+        niu.IdentityInterface(
+            fields=[
+                "source_file",
+                "fieldmap",
+                "fmap_coeff",
+                "fmap_ref",
+            ]),
+        name="inputnode"
+    )
+
+    ds_reference = pe.Node(
+        DerivativesDataSink(
+            base_directory=output_dir,
+            desc="reference",
+            suffix="fieldmap",
+            compress=True),
+        name="ds_reference",
+    )
+
+    ds_fieldmap = pe.Node(
+        DerivativesDataSink(
+            base_directory=output_dir,
+            desc="preproc",
+            suffix="fieldmap",
+            compress=True),
+        name="ds_fieldmap",
+    )
+    ds_fieldmap.inputs.Units = "Hz"
+
+    # fmt:off
+    workflow.connect([
+        (inputnode, ds_reference, [("source_file", "source_file"),
+                                   ("fmap_ref", "in_file")]),
+        (inputnode, ds_fieldmap, [("source_file", "source_file"),
+                                  ("fieldmap", "in_file")]),
+    ])
+    # fmt:on
+
+    if not write_coeff:
+        return workflow
+
+    ds_coeff = pe.MapNode(
+        DerivativesDataSink(
+            base_directory=output_dir,
+            suffix="fieldmap",
+            compress=True),
+        name="ds_coeff",
+        iterfield=("in_file", "desc"),
+    )
+
+    gen_desc = pe.Node(niu.Function(function=_gendesc), name="gen_desc")
+
+    # fmt:off
+    workflow.connect([
+        (inputnode, ds_coeff, [("source_file", "source_file"),
+                               ("fmap_coeff", "in_file")]),
+        (inputnode, gen_desc, [("fmap_coeff", "infiles")]),
+        (gen_desc, ds_coeff, [("out", "desc")]),
+    ])
+    # fmt:on
+
+    return workflow
+
+
+def init_sdc_unwarp_report_wf(name="sdc_unwarp_report_wf", forcedsyn=False):
     """
     Save a reportlet showing how SDC unwarping performed.
 
@@ -49,34 +147,53 @@ def init_sdc_unwarp_report_wf(name='sdc_unwarp_report_wf', forcedsyn=False):
 
     workflow = pe.Workflow(name=name)
 
-    inputnode = pe.Node(niu.IdentityInterface(
-        fields=['in_pre', 'in_post', 'in_seg', 'in_xfm']), name='inputnode')
-
-    map_seg = pe.Node(ApplyTransforms(
-        dimension=3, float=True, interpolation='MultiLabel'),
-        name='map_seg', mem_gb=0.3)
-
-    sel_wm = pe.Node(niu.Function(function=_dseg_label), name='sel_wm',
-                     mem_gb=DEFAULT_MEMORY_MIN_GB)
-    sel_wm.inputs.label = 2
-
-    bold_rpt = pe.Node(SimpleBeforeAfter(), name='bold_rpt',
-                       mem_gb=0.1)
-    ds_report_sdc = pe.Node(
-        DerivativesDataSink(desc=('sdc', 'forcedsyn')[forcedsyn], suffix='bold',
-                            datatype='figures'), name='ds_report_sdc',
-        mem_gb=DEFAULT_MEMORY_MIN_GB, run_without_submitting=True
+    inputnode = pe.Node(
+        niu.IdentityInterface(fields=["in_pre", "in_post", "in_seg", "in_xfm"]),
+        name="inputnode",
     )
 
+    map_seg = pe.Node(
+        ApplyTransforms(dimension=3, float=True, interpolation="MultiLabel"),
+        name="map_seg",
+        mem_gb=0.3,
+    )
+
+    sel_wm = pe.Node(
+        niu.Function(function=_dseg_label), name="sel_wm", mem_gb=DEFAULT_MEMORY_MIN_GB
+    )
+    sel_wm.inputs.label = 2
+
+    bold_rpt = pe.Node(SimpleBeforeAfter(), name="bold_rpt", mem_gb=0.1)
+    ds_report_sdc = pe.Node(
+        DerivativesDataSink(
+            desc=("sdc", "forcedsyn")[forcedsyn], suffix="bold", datatype="figures"
+        ),
+        name="ds_report_sdc",
+        mem_gb=DEFAULT_MEMORY_MIN_GB,
+        run_without_submitting=True,
+    )
+
+    # fmt: off
     workflow.connect([
-        (inputnode, bold_rpt, [('in_post', 'after'),
-                               ('in_pre', 'before')]),
-        (bold_rpt, ds_report_sdc, [('out_report', 'in_file')]),
-        (inputnode, map_seg, [('in_post', 'reference_image'),
-                              ('in_seg', 'input_image'),
-                              ('in_xfm', 'transforms')]),
-        (map_seg, sel_wm, [('output_image', 'in_seg')]),
-        (sel_wm, bold_rpt, [('out', 'wm_seg')]),
+        (inputnode, bold_rpt, [("in_post", "after"),
+                               ("in_pre", "before")]),
+        (bold_rpt, ds_report_sdc, [("out_report", "in_file")]),
+        (inputnode, map_seg, [("in_post", "reference_image"),
+                              ("in_seg", "input_image"),
+                              ("in_xfm", "transforms")]),
+        (map_seg, sel_wm, [("output_image", "in_seg")]),
+        (sel_wm, bold_rpt, [("out", "wm_seg")]),
     ])
+    # fmt: on
 
     return workflow
+
+
+def _gendesc(infiles):
+    if isinstance(infiles, (str, bytes)):
+        infiles = [infiles]
+
+    if len(infiles) == 1:
+        return "coeff"
+
+    return [f"coeff{i}" for i, _ in enumerate(infiles)]
