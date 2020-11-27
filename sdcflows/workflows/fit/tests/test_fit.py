4636791f3cf805bfabf827f9fd3a9134e85bc02d
Oscar Esteban <code@oscaresteban.es>
2020-11-27 18:13:39 +0100

    enh: refactor the fieldmapless-SyN method

--- sdcflows/workflows/fit/tests/test_fit.py
+++ sdcflows/workflows/fit/tests/test_fit.py
@@ -10,12 +10,12 @@ from .. import syn  # noqa
 @pytest.mark.parametrize(
     "workflow,kwargs",
     (
-        ("sdcflows.workflows.fit.pepolar.init_topup_wf", {}),
-        ("sdcflows.workflows.fit.pepolar.init_3dQwarp_wf", {}),
-        ("sdcflows.workflows.fit.syn.init_syn_sdc_wf", {}),
+        ("sdcflows.workflows.fit.fieldmap.init_fmap_wf", {"mode": "mapped"}),
         ("sdcflows.workflows.fit.fieldmap.init_fmap_wf", {}),
-        ("sdcflows.workflows.fit.fieldmap.init_fmap_wf", {"mode": "fieldmap"}),
         ("sdcflows.workflows.fit.fieldmap.init_phdiff_wf", {"omp_nthreads": 1}),
+        ("sdcflows.workflows.fit.pepolar.init_3dQwarp_wf", {}),
+        ("sdcflows.workflows.fit.pepolar.init_topup_wf", {}),
+        ("sdcflows.workflows.fit.syn.init_syn_sdc_wf", {"omp_nthreads": 1}),
     ),
 )
 def test_build_1(workflow, kwargs):
