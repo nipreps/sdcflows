ebc926db77536a2fee4f48aa03c85f9f17cf6d62
Oscar Esteban <code@oscaresteban.es>
2023-04-27 15:40:19 +0200

    wip: add a test

--- sdcflows/utils/tests/test_tools.py
+++ sdcflows/utils/tests/test_tools.py
@@ -22,8 +22,10 @@
 #
 """Test EPI manipulation routines."""
 import numpy as np
+import pytest
 import nibabel as nb
-from ..tools import brain_masker
+from nitransforms.linear import Affine
+from sdcflows.utils.tools import brain_masker, deoblique_and_zooms
 
 
 def test_epi_mask(tmpdir, testdata_dir):
@@ -31,3 +33,36 @@ def test_epi_mask(tmpdir, testdata_dir):
     tmpdir.chdir()
     mask = brain_masker(testdata_dir / "epi.nii.gz")[-1]
     assert abs(np.asanyarray(nb.load(mask).dataobj).sum() - 166476) < 10
+
+
+@pytest.mark.parametrize("padding", [0, 1, 4])
+@pytest.mark.parametrize("factor", [1, 4, 0.5])
+def test_deoblique_and_zooms(padding, factor):
+    """Check deoblique and denser."""
+
+    # Generate an example reference image
+    ref_data = np.zeros((20, 30, 40), dtype=np.float32)
+    ref_affine = np.eye(4)
+    ref_affine[:3, :3] = np.diag([1.0, 1.2, 0.8])  # Set zooms to (2, 3, 4)
+    ref_img = nb.Nifti1Image(ref_data, ref_affine)
+    ref_zooms = np.array(ref_img.header.get_zooms()[:3])
+
+    # Generate an example oblique image
+    ob_data = np.ones_like(ref_data)
+    rotate = np.eye(4)
+
+    # Rotate 90 degrees around x-axis
+    rotate[:3, :3] = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
+    ob_img = nb.Nifti1Image(ob_data, rotate @ ref_affine)
+
+    # Call function with default parameters
+    out_img = deoblique_and_zooms(ref_img, ob_img, padding=padding, factor=factor)
+
+    # Check output shape and zooms
+    assert np.allclose(out_img.header.get_zooms()[:3], ref_zooms / factor)
+
+    resampled = Affine(reference=out_img).apply(ob_img, order=0)
+    ref_volume = ref_data.size * ref_zooms.prod()
+    res_volume = resampled.get_fdata().sum() * np.prod(resampled.header.get_zooms())
+    # 20% of change in volume is too high, must be an error somewhere
+    assert abs(ref_volume - res_volume) < ref_volume * 0.2
