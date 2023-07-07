dc5bfaef329728c4f3723377ee32007f01a10abb
Chris Markiewicz <markiewicz@stanford.edu>
2023-07-07 16:47:08 -0400

    Remove pdb trace

--- sdcflows/transform.py
+++ sdcflows/transform.py
@@ -273,7 +273,6 @@ class B0FieldTransform:
             ``False`` if cache was valid and will be reused.
 
         """
-        import pdb; pdb.set_trace()
         # Calculate the physical coordinates of target grid
         if isinstance(target_reference, (str, bytes, Path)):
             target_reference = nb.load(target_reference)
