d65c845fa5344f5e30911b17b493086663f7ed24
Oscar Esteban <code@oscaresteban.es>
2023-11-28 17:17:05 +0100

    enh: populate CLI with BIDS-Apps options

--- sdcflows/cli/main.py
+++ sdcflows/cli/main.py
@@ -21,53 +21,230 @@
 #     https://www.nipreps.org/community/licensing/
 #
 """Standalone command line executable for estimation of fieldmaps."""
-import argparse
+import re
+from argparse import Action, ArgumentDefaultsHelpFormatter, ArgumentParser
+from functools import partial
 from pathlib import Path
 
+from sdcflows import __version__ as thisversion
 
-def _drop_sub(value):
-    return value[4:] if value.startswith("sub-") else value
+
+def _parse_participant_labels(value):
+    """
+    Drop ``sub-`` prefix of participant labels.
+
+    >>> _parse_participant_labels("s060")
+    ['s060']
+    >>> _parse_participant_labels("sub-s060")
+    ['s060']
+    >>> _parse_participant_labels("s060 sub-s050")
+    ['s050', 's060']
+    >>> _parse_participant_labels("s060 sub-s060")
+    ['s060']
+    >>> _parse_participant_labels("s060\tsub-s060")
+    ['s060']
+
+    """
+    return sorted(
+        set(
+            re.sub(r"^sub-", "", item.strip())
+            for item in re.split(r"\s+", f"{value}".strip())
+        )
+    )
 
 
 def _parser():
-    parser = argparse.ArgumentParser(
-        description="Parse a BIDS directory and show what estimators are available",
-        formatter_class=argparse.RawDescriptionHelpFormatter,
+    class ParticipantLabelAction(Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            setattr(namespace, self.dest, _parse_participant_labels(" ".join(values)))
+
+    def _path_exists(path, parser):
+        """Ensure a given path exists."""
+        if path is None or not Path(path).exists():
+            raise parser.error(f"Path does not exist: <{path}>.")
+        return Path(path).expanduser().absolute()
+
+    def _min_one(value, parser):
+        """Ensure an argument is not lower than 1."""
+        value = int(value)
+        if value < 1:
+            raise parser.error("Argument can't be less than one.")
+        return value
+
+    def _to_gb(value):
+        scale = {"G": 1, "T": 10**3, "M": 1e-3, "K": 1e-6, "B": 1e-9}
+        digits = "".join([c for c in value if c.isdigit()])
+        n_digits = len(digits)
+        units = value[n_digits:] or "G"
+        return int(digits) * scale[units[0]]
+
+    def _bids_filter(value):
+        from json import loads
+
+        if value and Path(value).exists():
+            return loads(Path(value).read_text())
+
+    parser = ArgumentParser(
+        description=f"""\
+SDCFlows {thisversion}
+
+Estimate fieldmaps available in a BIDS-compliant MRI dataset.""",
+        formatter_class=ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("bids_dir", type=Path, help="The input BIDS directory to parse")
+    PathExists = partial(_path_exists, parser=parser)
+    PositiveInt = partial(_min_one, parser=parser)
+
     parser.add_argument(
+        "bids_dir",
+        action="store",
+        type=PathExists,
+        help="The root folder of a BIDS valid dataset (sub-XXXXX folders should "
+        "be found at the top level in this folder).",
+    )
+    parser.add_argument(
+        "output_dir",
+        action="store",
+        type=Path,
+        help="The directory where the output files "
+        "should be stored. If you are running group level analysis "
+        "this folder should be prepopulated with the results of the "
+        "participant level analysis.",
+    )
+    parser.add_argument(
+        "analysis_level",
+        action="store",
+        nargs="+",
+        help="Level of the analysis that will be performed. "
+        "Multiple participant level analyses can be run independently "
+        "(in parallel) using the same output_dir.",
+        choices=["participant", "group"],
+    )
+
+    # optional arguments
+    parser.add_argument(
+        "--version", action="version", version=f"SDCFlows {thisversion}"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose_count",
+        action="count",
+        default=0,
+        help="Increases log verbosity for each occurrence, debug level is -vvv.",
+    )
+
+    # main options
+    g_bids = parser.add_argument_group("Options related to BIDS")
+    g_bids.add_argument(
+        "--participant-label",
+        "--participant_label",
+        "--participant-labels",
+        "--participant_labels",
+        dest="participant_label",
+        action=ParticipantLabelAction,
+        nargs="+",
+        help="A space delimited list of participant identifiers or a single "
+        "identifier (the sub- prefix can be removed).",
+    )
+    g_bids.add_argument(
+        "--bids-filter-file",
+        action="store",
+        type=Path,
+        metavar="PATH",
+        help="a JSON file describing custom BIDS input filter using pybids "
+        "{<suffix>:{<entity>:<filter>,...},...} "
+        "(https://github.com/bids-standard/pybids/blob/master/bids/layout/config/bids.json)",
+    )
+    g_bids.add_argument(
+        "--bids-database-dir",
+        metavar="PATH",
+        type=PathExists,
+        help="Path to an existing PyBIDS database folder, for faster indexing "
+        "(especially useful for large datasets).",
+    )
+
+    # General performance
+    g_perfm = parser.add_argument_group("Options to handle performance")
+    g_perfm.add_argument(
+        "--nprocs",
+        "--n_procs",
+        "--n_cpus",
+        "-n-cpus",
+        action="store",
+        type=PositiveInt,
+        help="""\
+Maximum number of simultaneously running parallel processes executed by *MRIQC* \
+(e.g., several instances of ANTs' registration). \
+However, when ``--nprocs`` is greater or equal to the ``--omp-nthreads`` option, \
+it also sets the maximum number of threads that simultaneously running processes \
+may aggregate (meaning, with ``--nprocs 16 --omp-nthreads 8`` a maximum of two \
+8-CPU-threaded processes will be running at a given time). \
+Under this mode of operation, ``--nprocs`` sets the maximum number of processors \
+that can be assigned work within an *MRIQC* job, which includes all the processors \
+used by currently running single- and multi-threaded processes. \
+If ``None``, the number of CPUs available will be automatically assigned (which may \
+not be what you want in, e.g., shared systems like a HPC cluster.""",
+    )
+    g_perfm.add_argument(
+        "--omp-nthreads",
+        "--ants-nthreads",
+        action="store",
+        type=PositiveInt,
+        help="""\
+Maximum number of threads that multi-threaded processes executed by *MRIQC* \
+(e.g., ANTs' registration) can use. \
+If ``None``, the number of CPUs available will be automatically assigned (which may \
+not be what you want in, e.g., shared systems like a HPC cluster.""",
+    )
+    g_perfm.add_argument(
+        "--mem",
+        "--mem_gb",
+        "--mem-gb",
+        dest="memory_gb",
+        action="store",
+        type=_to_gb,
+        help="Upper bound memory limit for MRIQC processes.",
+    )
+    g_perfm.add_argument(
+        "--testing",
+        dest="debug",
+        action="store_true",
+        default=False,
+        help="Use testing settings for a minimal footprint.",
+    )
+
+    g_perfm.add_argument(
+        "--pdb",
+        dest="pdb",
+        action="store_true",
+        default=False,
+        help="Open Python debugger (pdb) on exceptions.",
+    )
+
+    # Control instruments
+    g_outputs = parser.add_argument_group("Instrumental options")
+    g_outputs.add_argument(
+        "-w",
+        "--work-dir",
+        action="store",
+        type=Path,
+        default=Path("work").absolute(),
+        help="Path where intermediate results should be stored.",
+    )
+    g_outputs.add_argument(
         "-n",
         "--dry-run",
         action="store_true",
         default=False,
         help="only find estimable fieldmaps (that is, estimation is not triggered)",
     )
-    parser.add_argument(
-        "-s",
-        "--subjects",
-        type=_drop_sub,
-        nargs="+",
-        help="One or more subject identifiers",
-    )
-    parser.add_argument(
+    g_outputs.add_argument(
         "--fmapless",
         action="store_true",
         default=False,
         help="Allow fieldmap-less estimation",
     )
-    parser.add_argument(
-        "--bids-database-dir",
-        metavar="PATH",
-        type=Path,
-        help="Path to a PyBIDS database folder, for faster indexing (especially "
-        "useful for large datasets). Will be created if not present.",
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        help="Print information while finding estimators (Useful for debugging)",
-    )
+
     return parser
 
 
@@ -103,6 +280,7 @@ def main(argv=None):
     """
     Parse a BIDS directory and print subject estimators
     """
+    from logging import DEBUG
     from niworkflows.utils.bids import collect_participants
     from sdcflows.utils.wrangler import find_estimators
     from sdcflows.utils.misc import create_logger
@@ -111,8 +289,10 @@ def main(argv=None):
 
     bids_dir = pargs.bids_dir.resolve(strict=True)
     layout = gen_layout(bids_dir, pargs.bids_database_dir)
-    subjects = collect_participants(layout, pargs.subjects)
-    logger = create_logger("sdcflow.wrangler", level=10 if pargs.verbose else 40)
+    subjects = collect_participants(layout, pargs.participant_label)
+    logger = create_logger(
+        "sdcflow.wrangler", int(max(25 - 5 * pargs.verbose_count, DEBUG))
+    )
     estimators_record = {}
     for subject in subjects:
         estimators_record[subject] = find_estimators(
