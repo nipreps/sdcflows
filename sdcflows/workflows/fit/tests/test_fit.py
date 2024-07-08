dd5f251ccacb9c3e8cc290ef95ac98ddc5902f35
Patrick Sadil <psadil@gmail.com>
2024-07-08 10:34:55 -0400

    Improve test coverage

--- sdcflows/workflows/fit/tests/test_fit.py
+++ sdcflows/workflows/fit/tests/test_fit.py
@@ -34,8 +34,12 @@ from .. import syn  # noqa
     (
         ("sdcflows.workflows.fit.fieldmap.init_fmap_wf", {"mode": "mapped"}),
         ("sdcflows.workflows.fit.fieldmap.init_fmap_wf", {}),
-        ("sdcflows.workflows.fit.fieldmap.init_phdiff_wf", {"omp_nthreads": 1}),
+        (
+            "sdcflows.workflows.fit.fieldmap.init_phdiff_wf",
+            {"omp_nthreads": 1},
+        ),
         ("sdcflows.workflows.fit.pepolar.init_3dQwarp_wf", {}),
+        ("sdcflows.workflows.fit.pepolar.init_3dQwarp_wf", {"sloppy": True}),
         ("sdcflows.workflows.fit.pepolar.init_topup_wf", {}),
         ("sdcflows.workflows.fit.syn.init_syn_sdc_wf", {"omp_nthreads": 1}),
     ),
