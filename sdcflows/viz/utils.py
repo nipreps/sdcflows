f4182431412af0e8a978ec9e8ac40275bec32870
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-03-21 08:17:22 -0400

    fix: Use nireports-vendored svgutils library

--- sdcflows/viz/utils.py
+++ sdcflows/viz/utils.py
@@ -46,7 +46,7 @@ def plot_registration(
     from lxml import etree
     import matplotlib.pyplot as plt
     from nilearn.plotting import plot_anat
-    from svgutils.transform import SVGFigure
+    from nireports._vendored.svgutils.transform import SVGFigure
     from nireports.reportlets.utils import robust_set_limits, extract_svg, SVGNS
 
     plot_params = plot_params or {}
